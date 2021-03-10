567aca9f8e3e0749a8772abbb1054ea9c162f13c
gadial <gadial@gmail.com>
2021-03-10 19:06:32 +0200

    Fixing two small bugs in the handling of circuit options in base_experiment (#11)

--- qiskit_experiments/base_experiment.py
+++ qiskit_experiments/base_experiment.py
@@ -93,7 +93,7 @@ class BaseExperiment(ABC):
                 raise QiskitError("Duplicate qubits in physical qubits list.")
 
         # Store options and values
-        self._circuit_options = set(circuit_options) if circuit_options else None
+        self._circuit_options = set(circuit_options) if circuit_options else set()
 
     def run(self, backend, experiment_data=None, **kwargs):
         """Run an experiment and perform analysis.
@@ -119,7 +119,7 @@ class BaseExperiment(ABC):
         run_options = self.__run_defaults__.copy()
         circuit_options = {}
         for key, value in kwargs.items():
-            if key in _TRANSPILE_OPTIONS or self._circuit_options:
+            if key in _TRANSPILE_OPTIONS or key in self._circuit_options:
                 circuit_options[key] = value
             else:
                 run_options[key] = value
