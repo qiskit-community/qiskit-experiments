15880407ab9dc4ccf650d6e4583ece1ab79ecdf6
Daniel Egger <deg@zurich.ibm.com>
2021-03-18 20:44:20 +0100

    * Added tests.

--- test/calibration/test_data_processing.py
+++ test/calibration/test_data_processing.py
@@ -12,39 +12,84 @@
 
 """Data processor tests."""
 
+from qiskit_experiments import ExperimentData
+from qiskit_experiments.base_experiment import BaseExperiment
 from qiskit_experiments.calibration import DataProcessor
-from qiskit_experiments.calibration.metadata import CalibrationMetadata
-from qiskit_experiments.calibration.data_processing import SystemKernel, SystemDiscriminator
-from qiskit.result import models
+from qiskit_experiments.calibration.data_processing import (SystemKernel, SystemDiscriminator,
+                                                            ToReal, ToImag, Population)
+from qiskit.result.models import ExperimentResultData, ExperimentResult
 from qiskit.result import Result
 from qiskit.test import QiskitTestCase
 from qiskit.qobj.utils import MeasLevel
+from qiskit.qobj.common import QobjExperimentHeader
+
+
+class FakeExperiment(BaseExperiment):
+    """Fake experiment class for testing."""
+
+    def __init__(self):
+        """Initialise the fake experiment."""
+        self._type = None
+        super().__init__((0, ), 'fake_test_experiment')
+
+    def circuits(self, backend=None, **circuit_options):
+        """Fake circuits."""
+        return []
 
 
 class DataProcessorTest(QiskitTestCase):
     """Class to test DataProcessor."""
 
     def setUp(self):
+        """Setup variables used for testing."""
         self.base_result_args = dict(backend_name='test_backend',
                                      backend_version='1.0.0',
                                      qobj_id='id-123',
                                      job_id='job-123',
                                      success=True)
 
+        mem1 = ExperimentResultData(memory=[[[1103260.0, -11378508.0], [2959012.0, -16488753.0]],
+                                            [[442170.0, -19283206.0], [-5279410.0, -15339630.0]],
+                                            [[3016514.0, -14548009.0], [-3404756.0, -16743348.0]]])
+
+        mem2 = ExperimentResultData(memory=[[[5131962.0, -16630257.0], [4438870.0, -13752518.0]],
+                                            [[3415985.0, -16031913.0], [2942458.0, -15840465.0]],
+                                            [[5199964.0, -14955998.0], [4030843.0, -14538923.0]]])
+
+        header1 = QobjExperimentHeader(clbit_labels=[['meas', 0], ['meas', 1]],
+                                       creg_sizes=[['meas', 2]], global_phase=0.0, memory_slots=2,
+                                       metadata={'experiment_type': 'fake_test_experiment',
+                                                 'x_values': 0.0})
+
+        header2 = QobjExperimentHeader(clbit_labels=[['meas', 0], ['meas', 1]],
+                                       creg_sizes=[['meas', 2]], global_phase=0.0, memory_slots=2,
+                                       metadata={'experiment_type': 'fake_test_experiment',
+                                                 'x_values': 1.0})
+
+        res1 = ExperimentResult(shots=3, success=True, meas_level=1, data=mem1, header=header1)
+        res2 = ExperimentResult(shots=3, success=True, meas_level=1, data=mem2, header=header2)
+
+        self.result_lvl1 = Result(results=[res1, res2], **self.base_result_args)
+
         super().setUp()
 
     def test_empty_processor(self):
         """Check that a DataProcessor without steps does nothing."""
 
-        raw_counts = {'0x0': 4, '0x2': 10}
-        data = models.ExperimentResultData(counts=dict(**raw_counts))
-        exp_result = models.ExperimentResult(shots=14, success=True, meas_level=2, data=data)
-        result = Result(results=[exp_result], **self.base_result_args)
+        raw_counts = {'0x0': 4, '0x2': 5}
+        data = ExperimentResultData(counts=dict(**raw_counts))
+        header = QobjExperimentHeader(metadata={'experiment_type': 'fake_test_experiment'})
+        result_ = ExperimentResult(shots=9, success=True, meas_level=2, data=data, header=header)
+
+        result = Result(results=[result_], **self.base_result_args)
+
+        exp_data = ExperimentData(FakeExperiment())
+        exp_data.add_data(result)
 
         data_processor = DataProcessor()
-        processed = data_processor.format_data(result, metadata=CalibrationMetadata(), index=0)
-        self.assertEqual(processed.get_counts(0)['0'], 4)
-        self.assertEqual(processed.get_counts(0)['10'], 10)
+        data_processor.format_data(exp_data.data)
+        self.assertEqual(exp_data.data[0]['counts']['0'], 4)
+        self.assertEqual(exp_data.data[0]['counts']['10'], 5)
 
     def test_append_kernel(self):
         """Tests that we can add a kernel and a discriminator."""
@@ -56,3 +101,87 @@ class DataProcessorTest(QiskitTestCase):
 
         processor.append(SystemDiscriminator(None))
         self.assertEqual(processor.meas_level(), MeasLevel.CLASSIFIED)
+
+    def test_output_key(self):
+        """Test that we can properly get the output key from the node."""
+        processor = DataProcessor()
+        self.assertEqual(processor.output_key(), 'counts')
+
+        processor.append(SystemKernel())
+        self.assertEqual(processor.output_key(), 'memory')
+
+        processor.append(ToReal())
+        self.assertEqual(processor.output_key(), 'memory')
+
+        processor = DataProcessor()
+        processor.append(SystemKernel())
+        processor.append(SystemDiscriminator(None))
+        self.assertEqual(processor.output_key(), 'counts')
+
+        processor = DataProcessor()
+        processor.append(Population())
+        self.assertEqual(processor.output_key(), 'populations')
+
+    def test_to_real(self):
+        """Test scaling and conversion to real part."""
+        processor = DataProcessor()
+        processor.append(ToReal(scale=1e-3))
+        self.assertEqual(processor.output_key(), 'memory')
+
+        exp_data = ExperimentData(FakeExperiment())
+        exp_data.add_data(self.result_lvl1)
+
+        processor.format_data(exp_data.data[0])
+
+        expected = {'memory': [[1103.26, 2959.012], [442.17, -5279.41], [3016.514, -3404.7560]],
+                    'metadata': {'experiment_type': 'fake_test_experiment', 'x_values': 0.0}}
+
+        self.assertEqual(exp_data.data[0], expected)
+
+        # Test that we can average single-shots
+        processor = DataProcessor()
+        processor.append(ToReal(scale=1e-3, average=True))
+        self.assertEqual(processor.output_key(), 'memory')
+
+        exp_data = ExperimentData(FakeExperiment())
+        exp_data.add_data(self.result_lvl1)
+
+        processor.format_data(exp_data.data[0])
+
+        expected = {'memory': [1520.6480000000001, -1908.3846666666666],
+                    'metadata': {'experiment_type': 'fake_test_experiment', 'x_values': 0.0}}
+
+        self.assertEqual(exp_data.data[0], expected)
+
+    def test_to_imag(self):
+        """Test that we can average the data."""
+        processor = DataProcessor()
+        processor.append(ToImag(scale=1e-3))
+        self.assertEqual(processor.output_key(), 'memory')
+
+        exp_data = ExperimentData(FakeExperiment())
+        exp_data.add_data(self.result_lvl1)
+
+        processor.format_data(exp_data.data[0])
+
+        expected = {'memory': [[-11378.508, -16488.753],
+                               [-19283.206000000002, -15339.630000000001],
+                               [-14548.009, -16743.348]],
+                    'metadata': {'experiment_type': 'fake_test_experiment', 'x_values': 0.0}}
+
+        self.assertEqual(exp_data.data[0], expected)
+
+        # Test that we can average single-shots
+        processor = DataProcessor()
+        processor.append(ToImag(scale=1e-3, average=True))
+        self.assertEqual(processor.output_key(), 'memory')
+
+        exp_data = ExperimentData(FakeExperiment())
+        exp_data.add_data(self.result_lvl1)
+
+        processor.format_data(exp_data.data[0])
+
+        expected = {'memory': [-15069.907666666666, -16190.577],
+                    'metadata': {'experiment_type': 'fake_test_experiment', 'x_values': 0.0}}
+
+        self.assertEqual(exp_data.data[0], expected)
--- test/calibration/test_experiments.py
+++ test/calibration/test_experiments.py
@@ -12,6 +12,64 @@
 
 """Test the rough amplitude experiment."""
 
-class TestRoughAmplitude:
+from qiskit_experiments.calibration import CalibrationsDefinition
+from qiskit_experiments.calibration import ParameterValue
+from qiskit_experiments.calibration import RoughAmplitude
+import qiskit.pulse as pulse
+from qiskit.test import QiskitTestCase
+from qiskit.pulse import Drag, DriveChannel
+from qiskit.test.mock import FakeAlmaden
+from qiskit.circuit import Parameter, Gate
 
-    
+
+class TestCalibrationExperiments(QiskitTestCase):
+
+    """Class to test calibration experiments."""
+    def setUp(self):
+        """Setup variables used for testing."""
+        self.backend = FakeAlmaden()
+        self.cals = CalibrationsDefinition(self.backend)
+
+        sq_freq = Parameter('freq')
+        sigma_xp = Parameter('σ_xp')
+        d0 = Parameter('0')
+        amp_xp = Parameter('amp_xp')
+        amp_x90p = Parameter('amp_x90p')
+        amp_y90p = Parameter('amp_y90p')
+        beta_xp = Parameter('β_xp')
+
+        with pulse.build(name='xp') as xp:
+            pulse.play(Drag(160, amp_xp, sigma_xp, beta_xp), DriveChannel(d0))
+
+        with pulse.build(name='xm') as xm:
+            pulse.play(Drag(160, -amp_xp, sigma_xp, beta_xp), DriveChannel(d0))
+
+        with pulse.build(name='x90p') as x90p:
+            pulse.play(Drag(160, amp_x90p, sigma_xp, beta_xp), DriveChannel(d0))
+
+        with pulse.build(name='y90p') as y90p:
+            pulse.play(Drag(160, amp_y90p, sigma_xp, beta_xp), DriveChannel(d0))
+
+        self.cals.add_schedules([xp, x90p, y90p, xm])
+
+        self.cals.add_parameter_value('σ_xp', ParameterValue(40), ch_type=DriveChannel)
+        self.cals.add_parameter_value('amp_xp', ParameterValue(0.2), ch_type=DriveChannel)
+        self.cals.add_parameter_value('amp_x90p', ParameterValue(0.1), ch_type=DriveChannel)
+        self.cals.add_parameter_value('amp_y90p', ParameterValue(0.1j), ch_type=DriveChannel)
+        self.cals.add_parameter_value('β_xp', ParameterValue(0.0), ch_type=DriveChannel)
+
+    def test_rough_amplitude(self):
+        """The the rough amplitude calibration."""
+
+        qubit = 3
+        amps = [-0.5, 0.5]
+        amp = RoughAmplitude(qubit, self.cals, 'xp', 'amp_xp', amps)
+        circs = amp.transpiled_circuits(self.backend)
+
+        # Check that there is a gate on qubit 3.
+        self.assertEqual(circs[1].data[0][1][0].index, qubit)
+        self.assertTrue(isinstance(circs[1].data[0][0], Gate))
+
+        # Check that we have calibrations for the xp gate on qubit 3
+        for idx, amp in enumerate(amps):
+            self.assertTrue(((qubit, ), (amp, )) in circs[idx].calibrations['xp'])
