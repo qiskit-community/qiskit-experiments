669da4df2676ed08f84e5e74af7175ecca39cc6e
Daniel Egger <deg@zurich.ibm.com>
2021-03-18 20:42:18 +0100

    * Removed to dict method from CalibrationMetadata. * Added experiment_type to CalibrationMetadata.

--- qiskit_experiments/calibration/metadata.py
+++ qiskit_experiments/calibration/metadata.py
@@ -28,6 +28,9 @@ class CalibrationMetadata:
     # The name of the calibration experiment.
     name: str = None
 
+    # Type of the calibration experiment
+    experiment_type: str = None
+
     # Name of the pulse schedule that was used in the calibration experiment.
     pulse_schedule_name: str = None
 
@@ -44,19 +47,3 @@ class CalibrationMetadata:
 
     # Physical qubits used.
     qubits: List[int] = None
-
-    # Mapping of qubit index and classical register index.
-    # The key is the qubit index and value is the classical bit index.
-    # This mapping is automatically generated at BaseCalibrationExperiment class.
-    register_map: Dict[int, int] = None
-
-    def __post_init__(self):
-        """Ensure that keys are integers. This is needed because in JSon keys are str."""
-        if not self.register_map:
-            return
-
-        register_map = {}
-        for key, value in self.register_map.items():
-            register_map[int(key)] = int(value)
-
-        self.register_map = register_map
