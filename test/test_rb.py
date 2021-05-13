f37b00b34e068492f80d7365df137511c99ce21d
Itamar Goldman <Itamar.Goldman@ibm.com>
2021-05-13 12:29:45 +0300

    Create test_rb.py

--- test/test_rb.py
+++ test/test_rb.py
@@ -12,6 +12,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
+<<<<<<< HEAD
 """
 A Tester for the RB experiment
 """
@@ -37,10 +38,22 @@ from qiskit.circuit.library import (
 )
 from qiskit.providers.aer import AerSimulator
 import qiskit_experiments as qe
+=======
+import numpy as np
+import qiskit_experiments as qe
+from qiskit.quantum_info import Clifford
+from qiskit.test import QiskitTestCase
+from numpy.random import Generator, default_rng
+from typing import Union, Iterable, Optional
+from qiskit.test.mock import FakeParis
+import unittest
+from qiskit.exceptions import QiskitError
+>>>>>>> e966c18 (Create test_rb.py)
 
 
 @ddt
 class TestRB(QiskitTestCase):
+<<<<<<< HEAD
     """
     A test class for the RB Experiment to check that the RBExperiment class is working correctly.
     """
@@ -80,10 +93,29 @@ class TestRB(QiskitTestCase):
         Args:
             circuits (list): list of the circuits which we want to check
         """
+=======
+
+    @staticmethod
+    def RB_parameters_2_qubit():
+        exp_data = {'qubits': [0, 1], 'lengths': [1, 3, 5, 7, 9], 'num_samples': 1, 'seed': 100}
+        rb = qe.randomized_benchmarking
+        RB_Test = rb.RBExperiment(exp_data["qubits"], exp_data["lengths"], num_samples=exp_data["num_samples"],
+                                  seed=exp_data["seed"])
+        return exp_data, RB_Test
+
+    def is_identity(self, circuits):
+        """Standard randomized benchmarking test - Identity check (assuming all the operator are spanned by clifford group)
+        Args:
+            quantum_Circuits: list of the circuits which we want to check
+        """
+
+        identity = True
+>>>>>>> e966c18 (Create test_rb.py)
         for qc in circuits:
             num_qubits = qc.num_qubits
             qc.remove_final_measurements()
             # Checking if the matrix representation is the identity matrix
+<<<<<<< HEAD
             self.assertTrue(
                 matrix_equal(Clifford(qc).to_matrix(), np.identity(2 ** num_qubits)),
                 "Clifford sequence doesn't result in the identity matrix.",
@@ -236,3 +268,32 @@ class TestRBUtilities(QiskitTestCase):
             expected_epg = error_dict[((0,), gate)]
             actual_epg = epg[0][gate]
             self.assertTrue(np.allclose(expected_epg, actual_epg, rtol=1.0e-2))
+=======
+            self.assertEqual(np.allclose(Clifford(qc).to_matrix(), np.identity(2 ** num_qubits)),
+                             'Clifford sequence doesn\'t result in the identity matrix.')
+            # identity = identity and array_equal(a1, a2, equal_nan=False)
+        # self.assertEqual(identity, True,'Clifford sequence doesn\'t result in the identity matrix.')
+
+    def validate_metadata(self, circuits, exp_data: dict):
+        """
+
+        Args:
+            circuits:
+            exp_data: 
+
+        Returns:
+
+        """
+        for ind, qc in enumerate(circuits):
+            self.assertEqual(qc.metadata['xdata'], self._lengths[ind],
+                             'The length of the experiment doen\'t match to the one provided.')
+            self.assertEqual(qc.metadata['qubits'], tuple(self._qubits[ind]),
+                             'The qubits indices doesn\'t match the ran qubit indices.')
+
+    def test_RB_circuits(self):
+        exp_2_qubit_data_dict, exp_2_qubit_exp = self.RB_parameters_2_qubit()
+        exp_2_qubit_circ = exp_2_qubit_exp.circuits()
+        self.is_identity(exp_2_qubit_circ)
+        self.validate_metadata(exp_2_qubit_circ, exp_2_qubit_data_dict)
+
+>>>>>>> e966c18 (Create test_rb.py)
