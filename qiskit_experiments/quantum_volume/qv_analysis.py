4b7791578080ae9de79eee9187f2b7f40c3c33e9
dekel.meirom <dekel.meirom@ibm.com>
2021-05-16 11:44:26 +0300

    add plot

--- qiskit_experiments/quantum_volume/qv_analysis.py
+++ qiskit_experiments/quantum_volume/qv_analysis.py
@@ -16,53 +16,72 @@ Quantum Volume analysis class.
 import math
 import warnings
 import numpy as np
+from typing import Optional
 
 from qiskit_experiments.base_analysis import BaseAnalysis
+from qiskit_experiments.base_analysis import AnalysisResult
+from qiskit_experiments.analysis.plotting import plot_curve_fit, plot_scatter, plot_errorbar
+
+try:
+    from matplotlib import pyplot as plt
+    HAS_MATPLOTLIB = True
+except ImportError:
+    HAS_MATPLOTLIB = False
 
 class QVAnalysis(BaseAnalysis):
     """RB Analysis class."""
 
     # pylint: disable = arguments-differ
-    def _run_analysis(self, experiment_data):
+    def _run_analysis(self,
+                      experiment_data,
+                      plot: bool = True,
+                      ax: Optional["AxesSubplot"] = None
+                      ):
         """Run analysis on circuit data.
         Args:
             experiment_data (ExperimentData): the experiment data to analyze.
+            plot: If True generate a plot of fitted data.
+            ax: Optional, matplotlib axis to add plot to.
         Returns:
             tuple: A pair ``(analysis_result, figures)`` where
                    ``analysis_results`` may be a single or list of
                    AnalysisResult objects, and ``figures`` may be
                    None, a single figure, or a list of figures.
         """
-        depth = experiment_data.experiment.num_qubits
-        num_trials = experiment_data.experiment.trials
+        depth = experiment_data.experiment().num_qubits
+        num_trials = experiment_data.experiment().trials
 
-        heavy_outputs = np.zeros((depth, num_trials), dtype=float)
-        heavy_output_prob_exp = np.zeros((depth, num_trials), dtype=float)
+        heavy_outputs = np.zeros(num_trials, dtype=list)
+        heavy_output_prob_exp = np.zeros(num_trials, dtype=list)
 
         # analyse ideal data to calculate all heavy outputs
+        # must calculate first the ideal data, because the non-ideal calculation uses it
         for ideal_data in experiment_data.data:
             if not ideal_data["metadata"].get("is_simulation", None):
                 continue
-            depth = ideal_data["metadata"]["depth"]
-            depthidx = depth - 1 # depth is starting from 1
             trial = ideal_data["metadata"]["trial"]
-            trialidx = trial - 1  # trial is starting from 1
+            trial_index = trial - 1 # trials starts from 1, so as index use trials - 1
 
-            heavy_outputs[depthidx, trialidx] = self._calc_ideal_heavy_output(ideal_data)
+            heavy_outputs[trial_index] = self._calc_ideal_heavy_output(ideal_data)
 
         # analyse non-ideal data
         for data in experiment_data.data:
             if data["metadata"].get("is_simulation", None):
                 continue
-            depth = data["metadata"]["depth"]
-            depthidx = depth - 1  # depth is starting from 1
             trial = data["metadata"]["trial"]
-            trialidx = trial - 1  # trial is starting from 1
+            trial_index = trial - 1  # trials starts from 1, so as index use trials - 1
 
-            heavy_output_prob_exp[depthidx, trialidx] = \
-                self._calc_exp_heavy_output_probability(data, heavy_outputs[depthidx, trialidx])
+            heavy_output_prob_exp[trial_index] = \
+                self._calc_exp_heavy_output_probability(data, heavy_outputs[trial_index])
 
-        return self._calc_quantum_volume(heavy_output_prob_exp, depth, num_trials)
+        analysis_result = AnalysisResult(
+            self._calc_quantum_volume(heavy_output_prob_exp, depth, num_trials))
+
+        if plot:
+            ax = self._format_plot(ax, analysis_result)
+            if HAS_MATPLOTLIB:
+                analysis_result.plt = plt
+        return analysis_result, None
 
     @staticmethod
     def _calc_ideal_heavy_output(ideal_data):
@@ -145,46 +164,91 @@ class QVAnalysis(BaseAnalysis):
 
         return confidence_level
 
-    def _calc_quantum_volume(self, heavy_output_prob_exp, last_depth, trials):
+    def _calc_quantum_volume(self, heavy_output_prob_exp, depth, trials):
         """
         calc the quantum volume of the analysed system.
         quantum volume is determined by the largest successful depth.
         A depth is successful if it has 'mean heavy-output probability' > 2/3 with confidence
         level > 0.977 (corresponding to z_value = 2).
+        we assume the error (standard deviation) of the heavy output probability is due to a
+        binomial distribution. standard deviation for binomial distribution is sqrt(np(1-p)),
+        where n is the number of trials and p is the success probability.
 
         Returns:
-            int: quantum volume
+            dict: quantum volume calculations -
+            the quantum volume,
+            whether the results passed the threshold,
+            the confidence fof the result,
+            the heavy output probability for each trial,
+            the mean heavy output probability,
+            the error of the heavy output probability,
+            the depth of the circuit,
+            the number of trials ran
         """
-        success_list = []
+        quantum_volume = 0
+        success = False
         confidence_level_threshold = self._calc_confidence_level(z_value=2)
-        means, sigmas = self._calc_statistics(heavy_output_prob_exp, last_depth, trials)
+        mean_hop = np.mean(heavy_output_prob_exp)
+        sigma_hop = mean_hop * ((1.0 - mean_hop) / trials) ** 0.5
 
-        max_success_depth = 0
-        for depthidx, depth in enumerate(range(1, last_depth + 1)):
-            z_value = self._calc_z_value(means[depthidx], sigmas[depthidx])
-            confidence_level = self._calc_confidence_level(z_value)
+        z_value = self._calc_z_value(mean_hop, sigma_hop)
+        confidence_level = self._calc_confidence_level(z_value)
+        if mean_hop > 2 / 3 and confidence_level > confidence_level_threshold:
+            quantum_volume = 2 ** depth
+            success = True
 
-            if means[depthidx] > 2 / 3 and confidence_level > confidence_level_threshold:
-                success_list.append(True)
-                max_success_depth = depth
-            else:
-                success_list.append(False)
-        return 2 ** max_success_depth
+        result = {
+            "quantum volume": quantum_volume,
+            "success": success,
+            "confidence list": confidence_level,
+            "heavy output probability": heavy_output_prob_exp,
+            "mean hop": mean_hop,
+            "sigma": sigma_hop,
+            "depth": depth,
+            "trials": trials
+        }
+        return result
 
     @staticmethod
-    def _calc_statistics(heavy_output_prob, depth, trials):
+    def _format_plot(ax, analysis_result):
         """
-        Convert the heavy outputs in the different trials into mean and error for plotting.
-
-        Here we assume the error is due to a binomial distribution.
-        Error (standard deviation) for binomial distribution is sqrt(np(1-p)),
-        where n is the number of trials and p is the success probability.
+        format the QV plot
+        Args:
+            ax: matplotlib axis to add plot to.
+            analysis_result: the results of the experimnt
+        Returns:
+            AxesSubPlot: the matplotlib axes containing the plot.
         """
-        mean_hop = np.zeros(depth, dtype=float)
-        sigma_hop = np.zeros(depth, dtype=float)
+        trial_list = np.arange(1, analysis_result["trials"] + 1)  # x data
 
-        for depthidx, depth in enumerate(range(1, depth + 1)):
-            mean_hop[depthidx] = np.mean(heavy_output_prob[depthidx, :])
-            sigma_hop[depthidx] = mean_hop[depthidx] * ((1.0 - mean_hop[depthidx]) / trials) ** 0.5
+        hop_accumulative = np.cumsum(analysis_result["heavy output probability"]) / trial_list
+        two_sigma = 2 * (hop_accumulative * (1 - hop_accumulative) / trial_list) ** 0.5
 
-        return mean_hop, sigma_hop
+        # plot inidivual HOP as scatter
+        ax = plot_scatter(trial_list, analysis_result["heavy output probability"], ax=ax,
+                          s=3, zorder=3, label='Individual HOP')
+        # plot accumulative HOP
+        ax.plot(trial_list, hop_accumulative, color='r', label='Cumulative HOP')
+        # plot two-sigma shaded area
+        ax = plot_errorbar(trial_list, hop_accumulative, two_sigma, ax=ax,
+                           fmt="none", ecolor='lightgray', elinewidth=20, capsize=0,
+                           alpha=0.5, label='2$\\sigma$')
+        # plot 2/3 success threshold
+        ax.axhline(2 / 3, color='k', linestyle='dashed', linewidth=1, label='Threshold')
+
+        ax.set_xlim(1, analysis_result["trials"] + 1)
+        ax.set_ylim(hop_accumulative[-1] - 4 * two_sigma[-1],
+                    hop_accumulative[-1] + 4 * two_sigma[-1])
+
+        ax.set_xlabel('Number of Trials', fontsize=14)
+        ax.set_ylabel('Heavy Output Probability', fontsize=14)
+
+        ax.set_title('Quantum Volume ' + str(2 ** analysis_result["depth"]) +
+                     ' - accumulative hop', fontsize = 14)
+
+        # re-arrange legend order
+        handles, labels = ax.get_legend_handles_labels()
+        handles = [handles[1], handles[2], handles[0], handles[3]]
+        labels = [labels[1], labels[2], labels[0], labels[3]]
+        ax.legend(handles, labels)
+        return ax
