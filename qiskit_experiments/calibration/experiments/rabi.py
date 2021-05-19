* Added test and fixed variance issue.

qiskit_experiments/calibration/experiments/rabi.py
@@ -97,9 +97,8 @@ class RabiAnalysis(BaseAnalysis):
                 raise ValueError("Unsupported measurement level.")
 
         y_sigmas = np.array([data_processor(datum) for datum in experiment_data.data()])
-        print(y_sigmas)
         y_max, y_min = max(y_sigmas[:, 0]), min(y_sigmas[:, 0])
-        sigmas = y_sigmas[:, 1] / (y_max - y_min)
+        sigmas = np.sqrt(y_sigmas[:, 1]) / (y_max - y_min)
         ydata = (y_sigmas[:, 0] - y_min) / (y_max - y_min)
         xdata = np.array([datum["metadata"]["xval"] for datum in experiment_data.data()])
 

test/calibration/experiments/test_rabi.py
@@ -59,13 +59,14 @@ class TestRabiAnalysis(QiskitTestCase):
         thetas = np.linspace(-1.5*np.pi, 1.5*np.pi, 51)
         amplitudes = np.linspace(-0.95, 0.95, 51)
 
-        data = self.simulate_experiment_data(thetas, amplitudes)
-        experiment_data.add_data(data)
+        for shots in [10, 1024]:
+            data = self.simulate_experiment_data(thetas, amplitudes, shots=shots)
+            experiment_data.add_data(data)
 
-        rabi_analysis = RabiAnalysis()
+            rabi_analysis = RabiAnalysis()
 
-        data_processor = DataProcessor("counts", [Probability(outcome="1")])
+            data_processor = DataProcessor("counts", [Probability(outcome="1")])
 
-        result = rabi_analysis.run(experiment_data, data_processor=data_processor, plot=False)
+            result = rabi_analysis.run(experiment_data, data_processor=data_processor, plot=False)
 
-        print(result)
+            self.assertEqual(result["quality"], "computer_good")