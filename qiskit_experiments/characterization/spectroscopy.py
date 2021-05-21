* Made sure data processor error returns the standard deviation. * Fixed error scalling issue in SVD.

qiskit_experiments/characterization/spectroscopy.py
@@ -125,7 +125,7 @@ class SpectroscopyAnalysis(BaseAnalysis):
         y_sigmas = np.array([data_processor(datum) for datum in experiment_data.data()])
         min_y, max_y = min(y_sigmas[:, 0]), max(y_sigmas[:, 0])
         ydata = (y_sigmas[:, 0] - min_y) / (max_y - min_y)
-        sigmas = np.sqrt(y_sigmas[:, 1]) / (max_y - min_y)
+        sigmas = y_sigmas[:, 1] / (max_y - min_y)
         xdata = np.array([datum["metadata"]["xval"] for datum in experiment_data.data()])
 
         # Fitting will not work if any sigmas are exactly 0.

qiskit_experiments/data_processing/nodes.py
@@ -186,7 +186,7 @@ class SVD(TrainableDataAction):
                 error_value = np.sqrt(
                     (error[idx][0] * np.cos(angle)) ** 2 + (error[idx][1] * np.sin(angle)) ** 2
                 )
-                processed_error.append(error_value)
+                processed_error.append(error_value / self.scales[idx])
 
         if len(processed_data) == 1:
             return processed_data[0], processed_error[0]
@@ -399,11 +399,11 @@ class Probability(DataAction):
                 adding the corresponding probabilities.
 
         Returns:
-            processed data: A dict with the populations.
+            processed data: A dict with the populations and standard deviation.
         """
 
         shots = sum(datum.values())
         p_mean = datum.get(self._outcome, 0.0) / shots
         p_var = p_mean * (1 - p_mean) / shots
 
-        return p_mean, p_var
+        return p_mean, np.sqrt(p_var)