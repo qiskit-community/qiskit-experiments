* Added test for the average on spectroscopy. * Added a bugfix in the SVD node.

qiskit_experiments/characterization/qubit_spectroscopy.py
@@ -151,12 +151,17 @@ class SpectroscopyAnalysis(BaseAnalysis):
         y_sigmas = np.array([data_processor(datum) for datum in experiment_data.data()])
         min_y, max_y = min(y_sigmas[:, 0]), max(y_sigmas[:, 0])
         ydata = (y_sigmas[:, 0] - min_y) / (max_y - min_y)
-        sigmas = y_sigmas[:, 1] / (max_y - min_y)
-        xdata = np.array([datum["metadata"]["xval"] for datum in experiment_data.data()])
 
-        # Fitting will not work if any sigmas are exactly 0.
-        if any(sigmas == 0.0):
+        # Sigmas may be None and fitting will not work if any sigmas are exactly 0.
+        try:
+            sigmas = y_sigmas[:, 1] / (max_y - min_y)
+            if any(sigmas == 0.0):
+                sigmas = None
+
+        except TypeError:
             sigmas = None
+
+        xdata = np.array([datum["metadata"]["xval"] for datum in experiment_data.data()])
 
         # Set the default options that depend on the y-data.
         if not offset_guess:

qiskit_experiments/data_processing/nodes.py
@@ -189,9 +189,15 @@ class SVD(TrainableDataAction):
                 processed_error.append(error_value / self.scales[idx])
 
         if len(processed_data) == 1:
-            return processed_data[0], processed_error[0]
+            if error is None:
+                return processed_data[0], None
+            else:
+                return processed_data[0], processed_error[0]
 
-        return np.array(processed_data), np.array(processed_error)
+        if error is None:
+            return np.array(processed_data), None
+        else:
+            return np.array(processed_data), np.array(processed_error)
 
     def train(self, data: List[Any]):
         """Train the SVD on the given data.

test/test_qubit_spectroscopy.py
@@ -44,7 +44,9 @@ class SpectroscopyBackend(IQTestBackend):
         super().__init__(iq_cluster_centers, iq_cluster_width)
 
     # pylint: disable = arguments-differ
-    def run(self, circuits, shots=1024, meas_level=MeasLevel.KERNELED, **options):
+    def run(
+        self, circuits, shots=1024, meas_level=MeasLevel.KERNELED, meas_return="single", **options
+    ):
         """Run the spectroscopy backend."""
 
         result = {
@@ -77,6 +79,10 @@ class SpectroscopyBackend(IQTestBackend):
                 run_result["data"] = {"counts": counts}
             else:
                 memory = [self._draw_iq_shot(prob) for _ in range(shots)]
+
+                if meas_return == "avg":
+                    memory = np.average(np.array(memory), axis=0).tolist()
+
                 run_result["data"] = {"memory": memory}
 
             result["results"].append(run_result)
@@ -137,3 +143,12 @@ class TestQubitSpectroscopy(QiskitTestCase):
         self.assertTrue(result["value"] < 5.1e6)
         self.assertTrue(result["value"] > 4.9e6)
         self.assertEqual(result["quality"], "computer_good")
+        self.assertTrue(result["ydata_err"] is not None)
+
+        spec.set_run_options(meas_return="avg")
+        result = spec.run(backend).analysis_result(0)
+
+        self.assertTrue(result["value"] < 5.1e6)
+        self.assertTrue(result["value"] > 4.9e6)
+        self.assertEqual(result["quality"], "computer_good")
+        self.assertTrue(result["ydata_err"] is None)