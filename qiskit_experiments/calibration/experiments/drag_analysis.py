882814a096d83961105175170426f9aa2b9bcffb
Daniel Egger <deg@zurich.ibm.com>
2021-06-30 19:06:03 +0200

    * Docstring.

--- qiskit_experiments/calibration/experiments/drag_analysis.py
+++ qiskit_experiments/calibration/experiments/drag_analysis.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Rough drag pulse calibration experiment."""
+"""DRAG pulse calibration experiment."""
 
 from typing import Any, Dict, List, Union
 import numpy as np
