d580adda1ec93846ea9d90427949bd0af7fa1af1
dekel.meirom <dekel.meirom@ibm.com>
2021-07-17 22:04:20 +0300

    fix assert check

--- test/quantum_volume/test_qv.py
+++ test/quantum_volume/test_qv.py
@@ -210,7 +210,14 @@ class TestQuantumVolume(QiskitTestCase):
         with open(os.path.join(dir_name, results_json_file), "r") as json_file:
             successful_results = json.load(json_file)
         for key, value in successful_results.items():
-            self.assertAlmostEqual(
-                exp_data.analysis_result(-1)[key], value,
-                "result " + str(key) + " is not the same as the " "pre-calculated analysis",
-            )
+            if isinstance(value, float):
+                self.assertAlmostEqual(
+                    exp_data.analysis_result(-1)[key],
+                    value,
+                    "result " + str(key) + " is not the same as the " "pre-calculated analysis",
+                )
+            else:
+                self.assertTrue(
+                    exp_data.analysis_result(-1)[key] == value,
+                    "result " + str(key) + " is not the same as the " "pre-calculated analysis",
+                )
