Added tests with osc_freq not 0. Fixed rotation angle for p gate to be proportional to delay

qiskit_experiments/characterization/t2ramsey.py
@@ -11,12 +11,14 @@
 # that they have been altered from the originals.
 """
 T2Ramsey Experiment class.
+
 """
 
 from typing import List, Optional, Union
 import numpy as np
 
 import qiskit
+from qiskit.utils import apply_prefix
 from qiskit.providers import Backend
 from qiskit.circuit import QuantumCircuit
 from qiskit_experiments.base_experiment import BaseExperiment
@@ -25,7 +27,7 @@ from .t2ramsey_analysis import T2RamseyAnalysis
 
 class T2Ramsey(BaseExperiment):
     """T2Ramsey class"""
-
+    
     __analysis_class__ = T2RamseyAnalysis
 
     def __init__(
@@ -67,7 +69,7 @@ class T2Ramsey(BaseExperiment):
             The experiment circuits
 
         Raises:
-            AttributeError: if unit is dt but dt parameter is missing in the backend configuration
+            AttributeError: if unit is 'dt', but 'dt' parameter is missing in the backend configuration
         """
         if self._unit == "dt":
             try:
@@ -75,12 +77,15 @@ class T2Ramsey(BaseExperiment):
             except AttributeError as no_dt:
                 raise AttributeError("Dt parameter is missing in backend configuration") from no_dt
 
+        conversion_factor = 1 if self._unit in ("s", "dt") else apply_prefix(1, self._unit)
         circuits = []
         for delay in self._delays:
             circ = qiskit.QuantumCircuit(1, 1)
             circ.h(0)
             circ.delay(delay, 0, self._unit)
-            circ.p(2 * np.pi * self._osc_freq, 0)
+            nosc = delay/self._delays[0]
+            rotation_angle = 2 * np.pi * self._osc_freq * nosc
+            circ.p(rotation_angle, 0)
             circ.barrier(0)
             circ.h(0)
             circ.barrier(0)

test/test_t2ramsey.py
@@ -164,6 +164,7 @@ class TestT2Ramsey(QiskitTestCase):
                 dt_factor = 1
             else:
                 dt_factor = apply_prefix(1, unit)
+            osc_freq = 0.1111
             estimated_t2ramsey = 20
             estimated_freq = 0.1
             # Set up the circuits
@@ -176,7 +177,7 @@ class TestT2Ramsey(QiskitTestCase):
                     (np.linspace(16.0, 45.0, num=59)).astype(float),
                 )
 
-            exp = T2Ramsey(qubit, delays, unit=unit)
+            exp = T2Ramsey(qubit, delays, unit=unit, osc_freq=osc_freq)
             default_p0 = {
                 "A": 0.5,
                 "t2ramsey": estimated_t2ramsey,
@@ -202,7 +203,7 @@ class TestT2Ramsey(QiskitTestCase):
 
             expdata = exp.run(
                 backend=backend,
-                shots=2000,
+                shots=2000
             )
             result = expdata.analysis_result(0)
             self.assertAlmostEqual(
@@ -226,10 +227,11 @@ class TestT2Ramsey(QiskitTestCase):
         t2ramsey = [30, 25]
         estimated_freq = [0.1, 0.12]
         delays = [list(range(1, 60)), list(range(1, 50))]
-        dt_factor = 1
+        dt_factor = 1E-6
+        osc_freq = 0.1111
 
-        exp0 = T2Ramsey(0, delays[0])
-        exp2 = T2Ramsey(2, delays[1])
+        exp0 = T2Ramsey(0, delays[0], osc_freq=osc_freq)
+        exp2 = T2Ramsey(2, delays[1], osc_freq=osc_freq)
         par_exp = ParallelExperiment([exp0, exp2])
 
         p0 = {
@@ -265,8 +267,9 @@ class TestT2Ramsey(QiskitTestCase):
         # First experiment
         qubit = 0
         delays0 = list(range(1, 60, 2))
+        osc_freq = 0.1111
 
-        exp0 = T2Ramsey(qubit, delays0, unit=unit)
+        exp0 = T2Ramsey(qubit, delays0, unit=unit, osc_freq=osc_freq)
         default_p0 = {
             "A": 0.5,
             "t2ramsey": estimated_t2ramsey,