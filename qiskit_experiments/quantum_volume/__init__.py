06af0de181092439da181072e2722a6efdc9fb0a
Shelly Garion <shelly@il.ibm.com>
2021-07-20 14:54:08 +0300

    add QV to docs

--- qiskit_experiments/quantum_volume/__init__.py
+++ qiskit_experiments/quantum_volume/__init__.py
@@ -10,6 +10,37 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-"""Quantum Volume Experiment Classes."""
+"""
+=======================================================================================
+Quantum Volume Experiment (:mod:`qiskit_experiments.quantum_volume`)
+=======================================================================================
+
+.. currentmodule:: qiskit_experiments.quantum_volume
+
+Quantum Volume (QV) is a single-number metric that can be measured using a concrete protocol
+on near-term quantum computers of modest size. The QV method quantifies the largest random circuit
+of equal width and depth that the computer successfully implements.
+Quantum computing systems with high-fidelity operations, high connectivity, large calibrated gate sets,
+and circuit rewriting toolchains are expected to have higher quantum volumes.
+See `Qiskit Textbook
+<https://qiskit.org/textbook/ch-quantum-hardware/measuring-quantum-volume.html>`_
+for an explanation on the QV method.
+
+Experiments
+===========
+.. autosummary::
+    :toctree: ../stubs/
+
+    QuantumVolume
+
+
+Analysis
+========
+
+.. autosummary::
+    :toctree: ../stubs/
+
+    QuantumVolumeAnalysis
+"""
 from .qv_experiment import QuantumVolume
 from .qv_analysis import QuantumVolumeAnalysis
