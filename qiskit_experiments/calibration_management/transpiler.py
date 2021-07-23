6e2215f7334bce85b4e64136bdeb86fccc29e53d
Daniel Egger <deg@zurich.ibm.com>
2021-07-23 17:17:27 +0200

    * Added flexibility to handle Calibrations instances or user provided schedule configs.

--- qiskit_experiments/calibration_management/transpiler.py
+++ qiskit_experiments/calibration_management/transpiler.py
@@ -12,9 +12,11 @@
 
 """Transpiler pass for calibration experiments."""
 
+from abc import abstractmethod
 from typing import Dict, List, Optional, Tuple, Union
 
 from qiskit import QuantumCircuit
+from qiskit.circuit import Parameter
 from qiskit.dagcircuit import DAGCircuit
 from qiskit.pulse.schedule import ScheduleBlock
 from qiskit.transpiler.basepasses import TransformationPass
@@ -26,95 +28,25 @@ from qiskit_experiments.calibration_management.calibration_key_types import Inst
 from qiskit_experiments.framework.base_experiment import BaseExperiment
 
 
-class CalAdder(TransformationPass):
+class BaseCalibrationAdder(TransformationPass):
     """Transformation pass to inject calibrations into circuits of calibration experiments."""
 
-    def __init__(
-            self,
-            calibrations: Calibrations,
-            instruction_maps: Optional[List[InstructionMap]] = None,
-            qubit_layout: Optional[Dict[int, int]] = None
-    ):
+    def __init__(self, qubit_layout: Optional[Dict[int, int]] = None):
         """Initialize the pass.
 
-        This transpiler pass is intended to be run in the :meth:`circuits` method of the
-        experiment classes before the main transpiler pass. It's only goal is to extract
-        the needed pulse schedules from an instance of Calibrations and attach them to the
-        template circuit. This has a couple of challenges.
-
-        * First, the same pulse in the calibrations can be attached to different gates.
-          For example, an X-gate "x" may need to be attached to a Rabi gate in a
-          :class:`Rabi` experiment while in an :class:`EFSpectroscopy` experiment it will
-          be attached to the X-gate.
-
-        * Second, the gate may sometimes be attached with parameters and sometimes not.
-          In a Rabi experiment the "x" schedule will have a parametric amplitude while in
-          the :class:`FineXAmplitude` the gate will not have any free parameters.
-
-        These two issues are solved by adding an InstructionMap which is a named tuple of
-        instruction name in the circuit, the schedule name in the calibrations and any
-        parameter instance that needs to be unassigned when getting the schedule from the
-        :class:`Calibrations` instance. Consider the following examples.
-
-        .. code-block::python
-
-            # Instruction mapping for a Rabi experiment
-            InstructionMap("Rabi", "x", [Parameter("amp")])
-
-            # Instruction mapping for a Drag experiment
-            beta = Parameter("β")
-            InstructionMap("Rp", "x", [beta])
-            InstructionMap("Rm", "xm", [beta])
-
-        Note that if no mapping is provided this transpiler pass assumes that the name of
-        the schedule in the calibrations is the same as the name of the gate instruction.
-
         Args:
-            calibrations: An instance of calibration from which to fetch the schedules.
-            instruction_maps: A list of instruction maps to map gate names in the circuit to
-                schedule names in the calibrations. If this is not provided the transpiler pass
-                will assume that the schedule has the same name as the gate. Each instruction map
-                may also specify parameters that should be left free in the schedule.
-            qubit_layout: The initial layout that will be used. This remaps the qubits
-                in the added calibrations. For instance, if {0: 3} is given and use this pass
-                on a circuit then any gates on qubit 0 will add calibrations for qubit 3.
+            qubit_layout: The initial layout to be used in the transpilation called before
+                running the experiment. This initial layout is needed here since the qubits
+                in the circuits on which this transpiler pass is run will always be [0, 1, ...].
+                If the initial layout is, for example, {0: 3} then any gates on qubit 0 will
+                add calibrations for qubit 3.
         """
         super().__init__()
-        self._cals = calibrations
-
-        self._instruction_maps = dict()
-        for inst_map in instruction_maps:
-            self._instruction_maps[inst_map.inst] = inst_map
-
         self._qubit_layout = qubit_layout
 
-    def get_calibration(
-            self,
-            gate_name: str,
-            qubits: Tuple[int, ...],
-    ) -> Union[ScheduleBlock, None]:
-        """Get a schedule from the calibrations.
-
-        Args:
-            gate_name: Name of the gate for which to get the schedule.
-            qubits: The qubits for which to get the parameters.
-
-        Returns:
-            The schedule if one is found otherwise return None.
-        """
-        name = gate_name
-        assign_params = None
-
-        # check for a non-trivial instruction to schedule mapping.
-        if gate_name in self._instruction_maps:
-            inst_map = self._instruction_maps[gate_name]
-            name = inst_map.schedule
-            assign_params = {param.name: param for param in inst_map.free_params}
-
-        try:
-            return self._cals.get_schedule(name, qubits, assign_params=assign_params)
-        except CalibrationError:
-            return None
+    @abstractmethod
+    def _get_calibration(self, gate: str, qubits: Tuple[int, ...]) -> Union[ScheduleBlock, None]:
+        """Get a schedule from the internally stored schedules."""
 
     def run(self, dag: DAGCircuit) -> DAGCircuit:
         """Run the calibration adder pass on `dag`.
@@ -130,37 +62,192 @@ class CalAdder(TransformationPass):
         for node in dag.nodes():
             if node.type == "op":
                 params = node.op.params
+
+                # Get the qubit indices in the circuit.
                 qubits = tuple(bit_indices[qarg] for qarg in node.qargs)
 
-                if self._qubit_layout is not None:
-                    try:
-                        qubits = tuple(self._qubit_layout[qubit] for qubit in qubits)
-                    except KeyError:
-                        pass
+                # Get the physical qubits that they will remap to.
+                qubits = tuple(self._qubit_layout[qubit] for qubit in qubits)
 
-                schedule = self.get_calibration(node.op.name, qubits)
+                schedule = self._get_calibration(node.op.name, qubits)
 
-                if schedule is not None:
-                    dag.add_calibration(node.op, qubits, schedule, params=params)
+                # Permissive stance: if we don't find a schedule we continue.
+                # The call to the transpiler that happens before running the
+                # experiment will either complain or force us to use the
+                # backend gates.
+                if schedule is None:
+                    continue
+
+                if len(set(qubits) & set(ch.index for ch in schedule.channels)) == 0:
+                    raise CalibrationError(
+                        f"None of the qubits {qubits} are contained in the channels of "
+                        f"the schedule named {schedule.name} for gate {node.op}."
+                    )
+
+                # Check the consistency between the circuit and schedule parameters
+                for param in params:
+                    if isinstance(param, Parameter) and param not in schedule.parameters:
+                        raise CalibrationError(
+                            f"Gate {node.op.name} has parameter {param} that is not contained "
+                            f"in schedule {schedule}."
+                        )
+
+                dag.add_calibration(node.op, qubits, schedule, params=params)
 
         return dag
 
 
-def inject_calibrations(circuit: QuantumCircuit, experiment: BaseExperiment) -> QuantumCircuit:
+class CalibrationsAdder(BaseCalibrationAdder):
+    """This BaseCalibrationAdder stores schedules in the calibrations.
+
+    This transpiler pass is intended to be run in the :meth:`circuits` method of the
+    experiment classes before the main transpiler pass. It's only goal is to extract
+    the needed pulse schedules from an instance of Calibrations and attach them to the
+    template circuit. This has a couple of challenges.
+
+    * First, the same pulse in the calibrations can be attached to different gates.
+      For example, an X-gate "x" may need to be attached to a Rabi gate in a
+      :class:`Rabi` experiment while in an :class:`EFSpectroscopy` experiment it will
+      be attached to the X-gate.
+
+    * Second, the gate may sometimes be attached with parameters and sometimes not.
+      In a Rabi experiment the "x" schedule will have a parametric amplitude while in
+      the :class:`FineXAmplitude` the gate will not have any free parameters.
+
+    These two issues are solved by adding an InstructionMap which is a named tuple of
+    instruction name in the circuit, the schedule name in the calibrations and any
+    parameter instance that needs to be unassigned when getting the schedule from the
+    :class:`Calibrations` instance. Consider the following examples.
+
+    .. code-block::python
+
+        # Instruction mapping for a Rabi experiment
+        InstructionMap("Rabi", "x", [Parameter("amp")])
+
+        # Instruction mapping for a Drag experiment
+        beta = Parameter("β")
+        InstructionMap("Rp", "x", [beta])
+        InstructionMap("Rm", "xm", [beta])
+
+    Note that if no mapping is provided this transpiler pass assumes that the name of
+    the schedule in the calibrations is the same as the name of the gate instruction.
+    """
+
+    def __init__(
+            self,
+            calibrations: Calibrations,
+            instruction_maps: Optional[List[InstructionMap]] = None,
+            qubit_layout: Optional[Dict[int, int]] = None,
+    ):
+        """Initialize the pass.
+
+        Args:
+            calibrations: An instance of calibration from which to fetch the schedules.
+            instruction_maps: A list of instruction maps to map gate names in the circuit to
+                schedule names in the calibrations. If this is not provided the transpiler pass
+                will assume that the schedule has the same name as the gate. Each instruction map
+                may also specify parameters that should be left free in the schedule.
+            qubit_layout: The initial layout that will be used.
+        """
+        super().__init__(qubit_layout)
+        self._cals = calibrations
+
+        self._instruction_maps = dict()
+        for inst_map in instruction_maps:
+            self._instruction_maps[inst_map.inst] = inst_map
+
+
+    def _get_calibration(self, gate: str, qubits: Tuple[int, ...]) -> Union[ScheduleBlock, None]:
+        """Get a schedule from the internally stored calibrations.
+
+        Args:
+            gate: Name of the gate for which to get the schedule.
+            qubits: The qubits for which to get the parameters.
+
+        Returns:
+            The schedule if one is found otherwise return None.
+        """
+        name = gate
+        assign_params = None
+
+        # check for a non-trivial instruction to schedule mapping.
+        if gate in self._instruction_maps:
+            inst_map = self._instruction_maps[gate]
+            name = inst_map.schedule
+            assign_params = {param.name: param for param in inst_map.free_params}
+
+        try:
+            return self._cals.get_schedule(name, qubits, assign_params=assign_params)
+        except CalibrationError:
+            return None
+
+
+class ScheduleAdder(BaseCalibrationAdder):
+    """A naive calibrations adder for lists of schedules.
+
+    This calibration adder is used for cases when users provide schedules for the
+    experiment as a dict of schedules.
+    """
+
+    def __init__(
+        self,
+        schedules: Dict[Tuple[str, Tuple[int, ...]], ScheduleBlock],
+        qubit_layout: Optional[Dict[int, int]] = None,
+    ):
+        """Initialize the :class:`ScheduleAdder` from a dict of schedules.
+
+        Args:
+            schedules: The schedules are provided as a dict. Here, the keys correspond to a
+                tuple of the name of the instruction in the quantum circuits and the qubits
+                while the values are the schedules that will be added to the calibrations of
+                the QuantumCircuit.
+            qubit_layout: The initial layout that will be used.
+        """
+        super().__init__(qubit_layout)
+        self._schedules = schedules
+
+    def _get_calibration(self, gate: str, qubits: Tuple[int, ...]) -> Union[ScheduleBlock, None]:
+        """Get a schedule from the internally stored schedules.
+
+        Args:
+            gate: Name of the gate for which to get the schedule.
+            qubits: The qubits for which to get the parameters.
+
+        Returns:
+            The schedule if one is found otherwise return None.
+        """
+        return self._schedules.get((gate, qubits), None)
+
+
+def inject_calibrations(
+    circuits: Union[QuantumCircuit, List[QuantumCircuit]],
+    experiment: BaseExperiment
+) -> Union[QuantumCircuit, List[QuantumCircuit]]:
     """Inject calibrations from a :class:`Calibrations` instance into a circuit.
 
     This function requires that the experiment has a list of InstructionMaps in its
     experiment options as well as the calibrations.
 
     Args:
-        circuit: The circuit into which to inject calibrations.
+        circuits: The circuit or list of circuits into which to inject calibrations.
         experiment: The experiment object that
 
     Returns:
          A quantum circuit with the relevant calibrations inject into it.
     """
-    calibrations = experiment.experiment_options.calibrations
-    inst_maps = experiment.experiment_options.instruction_name_maps
-    layout = {idx: qubit  for idx, qubit in enumerate(experiment.physical_qubits)}
+    layout = {idx: qubit for idx, qubit in enumerate(experiment.physical_qubits)}
 
-    return PassManager(CalAdder(calibrations, inst_maps, layout)).run(circuit)
+    calibrations = experiment.experiment_options.get("calibrations", None)
+
+    if calibrations is None:
+        user_schedule_config = experiment.experiment_options.get("schedules", None)
+
+        if user_schedule_config is None:
+            return circuits
+
+        return PassManager(ScheduleAdder(user_schedule_config, layout)).run(circuits)
+
+    else:
+        inst_maps = experiment.experiment_options.instruction_name_maps
+
+        return PassManager(CalibrationsAdder(calibrations, inst_maps, layout)).run(circuits)
