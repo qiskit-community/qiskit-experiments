10657fffc31b6b001a3db1e8512f7f22c6eb376b
Yael Ben-Haim <yaelbh@il.ibm.com>
2021-07-25 10:44:17 +0300

    black

--- qiskit_experiments/framework/base_experiment.py
+++ qiskit_experiments/framework/base_experiment.py
@@ -124,11 +124,15 @@ class BaseExperiment(ABC):
         # Scheduling parameters
         if backend.configuration().simulator is False and isinstance(backend, FakeBackend) is False:
             timing_constraints = getattr(self.transpile_options.__dict__, "timing_constraints", {})
-            timing_constraints["acquire_alignment"] = getattr(timing_constraints, "acquire_alignment", 16)
+            timing_constraints["acquire_alignment"] = getattr(
+                timing_constraints, "acquire_alignment", 16
+            )
             scheduling_method = getattr(
                 self.transpile_options.__dict__, "scheduling_method", "alap"
             )
-            self.set_transpile_options(timing_constraints=timing_constraints, scheduling_method=scheduling_method)
+            self.set_transpile_options(
+                timing_constraints=timing_constraints, scheduling_method=scheduling_method
+            )
 
         # Generate and transpile circuits
         transpile_opts = self.transpile_options.__dict__
