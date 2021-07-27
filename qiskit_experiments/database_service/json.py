80f46a0d2ddf9d43467eb780f4a643b94f9bc01b
Christopher Wood <cjwood@us.ibm.com>
2021-07-27 16:22:26 -0400

    Update json serializer for tomography types

--- qiskit_experiments/database_service/json.py
+++ qiskit_experiments/database_service/json.py
@@ -9,14 +9,17 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-# pylint: disable=method-hidden
+# pylint: disable=method-hidden,too-many-return-statements
 
 """Experiment serialization methods."""
 
 import json
+from types import FunctionType
 from typing import Any
 
 import numpy as np
+from qiskit.quantum_info.operators import Operator, Choi
+from qiskit.quantum_info.states import Statevector, DensityMatrix
 from .db_fitval import FitVal
 
 
@@ -29,13 +32,25 @@ class ExperimentEncoder(json.JSONEncoder):
         if isinstance(obj, complex):
             return {"__type__": "complex", "__value__": [obj.real, obj.imag]}
         if isinstance(obj, FitVal):
-            return {"__type__": obj.__class__.__name__, "args": (obj.value, obj.stderr, obj.unit)}
-        if callable(obj):
-            return {"__type__": "callable", "__value__": obj.__name__}
+            return {"__type__": type(obj).__name__, "args": (obj.value, obj.stderr, obj.unit)}
+        if isinstance(obj, (Operator, Choi)):
+            return {
+                "__type__": type(obj).__name__,
+                "args": (obj.data,),
+                "kwargs": {"input_dims": obj.input_dims(), "output_dims": obj.output_dims()},
+            }
+        if isinstance(obj, (Statevector, DensityMatrix)):
+            return {
+                "__type__": type(obj).__name__,
+                "args": (obj.data,),
+                "kwargs": {"dims": obj.dims()},
+            }
+        if isinstance(obj, FunctionType):
+            return {"__type__": "function", "__value__": obj.__name__}
         try:
             return super().default(obj)
         except TypeError:
-            return {"__type__": "__class_name__", "__value__": obj.__class__.__name__}
+            return {"__type__": "__class_name__", "__value__": type(obj).__name__}
 
 
 class ExperimentDecoder(json.JSONDecoder):
@@ -54,7 +69,13 @@ class ExperimentDecoder(json.JSONDecoder):
                 return np.array(obj["__value__"])
             if obj["__type__"] == FitVal.__name__:
                 return FitVal(*obj["args"])
-            if obj["__type__"] == "callable":
+            if obj["__type__"] == "Statevector":
+                return Statevector(obj["args"], **obj["kwargs"])
+            if obj["__type__"] == "DensityMatrix":
+                return DensityMatrix(obj["args"], **obj["kwargs"])
+            if obj["__type__"] == "Choi":
+                return Choi(obj["args"], **obj["kwargs"])
+            if obj["__type__"] == "function":
                 return obj["__value__"]
             if obj["__type__"] == "__class_name__":
                 return obj["__value__"]
