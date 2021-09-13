a2be5f8d5d9277b3bb1766304ec33f332a4e181e
Itamar Goldman <itamargoldman@gmail.com>
2021-09-13 16:05:52 +0300

    Pylint and Black

--- qiskit_experiments/library/characterization/T2Hahn.py
+++ qiskit_experiments/library/characterization/T2Hahn.py
@@ -1,28 +1,60 @@
-from typing import Union, Iterable, Optional, List
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+T2Hahn Echo Experiment class.
+
+"""
+
+from typing import Union, Iterable, List, Optional
 
 import numpy as np
-from numpy.random import Generator, default_rng
-
-from qiskit import QuantumCircuit, QiskitError, QuantumRegister, ClassicalRegister
-from qiskit.ignis.characterization.characterization_utils import pad_id_gates, time_to_ngates
-from qiskit.providers import Backend
-from qiskit.quantum_info import Clifford
+from qiskit import QuantumCircuit, QiskitError
+from qiskit.circuit import Measure
 from qiskit.providers.options import Options
-from qiskit.circuit import Gate
-
-from qiskit_experiments.framework import BaseExperiment, ParallelExperiment
-from qiskit_experiments.curve_analysis.data_processing import probability
-from qiskit.utils import apply_prefix
-
 from qiskit.providers import Backend
-from qiskit.test.mock import FakeParis
-from qiskit.providers.aer import AerSimulator
+
+from qiskit_experiments.framework import BaseExperiment
 
 
 class T2Hahn(BaseExperiment):
+    r"""T2 Ramsey Experiment.
 
-    # Analysis class for experiment
-    #     __analysis_class__ = T2Analysis  # need to add T2Analysis
+        # section: overview
+
+            This experiment is used to estimate T2 noise of a single qubit.
+
+            See `Qiskit Textbook <https://qiskit.org/textbook/ch-quantum-hardware/\
+            calibrating-qubits-pulse.html>`_  for a more detailed explanation on
+            these properties.
+
+            This experiment consists of a series of circuits of the form
+
+            .. parsed-literal::
+
+                 ┌─────────┐┌──────────┐┌───────┐┌──────────┐┌──────────┐┌─┐
+            q_0: ┤ RY(π/2) ├┤ DELAY(t) ├┤ RX(π) ├┤ DELAY(t) ├┤ RY(-π/2) ├┤M├
+                 └─────────┘└──────────┘└───────┘└──────────┘└──────────┘└╥┘
+            c: 1/═════════════════════════════════════════════════════════╩═
+                                                                         0
+
+            for each *t* from the specified delay times, where
+            :math:`\lambda =2 \pi \times {osc\_freq}`,
+            and the delays are specified by the user.
+            The circuits are run on the device or on a simulator backend.
+
+        # section: tutorial
+            :doc:`/tutorials/t2ramsey_characterization`
+
+        """
 
     @classmethod
     def _default_experiment_options(cls) -> Options:
@@ -31,137 +63,127 @@ class T2Hahn(BaseExperiment):
         Experiment Options:
             delays (Iterable[float]): Delay times of the experiments.
             unit (str): Unit of the delay times. Supported units are
-                's', 'ms', 'us', 'ns', 'ps', 'dt'.
-            osc_freq (float): Oscillation frequency offset in Hz.
+                's'.
             n_echos (int); Number of echoes to preform.
-
+            phase_alt_echo (bool): If to use alternate echoes (must have n_echoes greater than 1)
         """
         options = super()._default_experiment_options()
 
+        options.qubit = []
         options.delays = None
         options.unit = "s"
-        options.osc_freq = 0.0
-        options.n_echoes = 1
+        options.n_echos = 1
         options.phase_alt_echo = False
 
         return options
 
     def __init__(
-            self,
-            qubit: Union[int, Iterable[int]],
-            delays: Union[List[int], np.array],  # need to change name?
-            gate_time: float,
-            n_echos: int = 1,
-            phase_alt_echo: bool = False,
+        self,
+        qubit: Union[int, Iterable[int]],
+        delays: Union[List[float], np.array],  # need to change name?
+        n_echos: int = 1,
+        phase_alt_echo: bool = False,
     ):
-        """Initialize a T2 experiment with Hahn echo.
-
+        """
+        **T2 - Hahn Echo class**
+        Initialize the T2 - Hahn Echo class
          Args:
-                num_of_gates:
-                    Each element of the list corresponds to a circuit.
-                    `num_of_gates[i]` is the number of identity gates in each section
-                    "t" of the pulse sequence in circuit no. i.
-                    Must be in an increasing order.
-                gate_time: time of running a single identity gate.
-                qubits: indices of the qubits whose
-                    T\ :sub:`2`:sup:`*`\ 's are to be measured.
-                n_echos: number of echo gates (`X` or `Y`).
-                phase_alt_echo: if True then alternate the echo between
-                    `X` and `Y`.
+             qubit: the qubit under test.
+             delays (List[float)): delay times of the experiments.
+             n_echos (int): Amount of Echoes to preform.
+             phase_alt_echo (bool): if to use alternate echo methods
+
+         Raises:
+             Error for invalid input.
         """
         # Initialize base experiment
         super().__init__(qubit)
-        self._verify_parameters(qubit, delays, n_echos, gate_time)
-        self._qubit = qubit
-        self._delays = delays
-        self._gate_time = gate_time
-        self._n_echos = n_echos
-        self._phase_alt_echo = phase_alt_echo
-
         # Set configurable options
+        self.set_experiment_options(
+            delays=delays, n_echos=n_echos, phase_alt_echo=phase_alt_echo, qubit=qubit
+        )
+        self._verify_parameters()
 
-    #         self.set_experiment_options(delays=delays, n_echos=n_echos, phase_alt_echo=phase_alt_echo)
-    #         self.set_analysis_options(data_processor=probability(
-    #             outcome="0" * self.num_qubits))  # Need to rewrite after making the analysis class
+    def _verify_parameters(self):
+        """
+        Verify input correctness, raise QiskitError if needed.
+        Args:
+            qubit: the qubit under test.
 
-    @staticmethod
-    def _verify_parameters(self, qubit, delays, n_echos, gate_time):
-        """Verify input correctness, raise QiskitError if needed"""
-        if any(delay <= 0 for delay in delays):
+        Raises:
+            QiskitError : Error for invalid input.
+        """
+        if any(delay <= 0 for delay in self.experiment_options.delays):
             raise QiskitError(
-                f"The lengths list {delays} should only contain " "positive elements."
+                f"The lengths list {self.experiment_options.delays} should only contain "
+                "positive elements."
             )
-        if len(set(delays)) != len(delays):
+        if len(set(self.experiment_options.delays)) != len(self.experiment_options.delays):
             raise QiskitError(
-                f"The lengths list {delays} should not contain " "duplicate elements."
+                f"The lengths list {self.experiment_options.delays} should not contain "
+                "duplicate elements."
             )
-        if any(delays[idx - 1] >= delays[idx] for idx in range(1, len(delays))):
-            raise QiskitError(f"The number of identity gates {delays} should " "be increasing.")
 
-        # if any(qubit < 0 for qubit in qubits):
-        #     raise QiskitError(f"The index of the qubits {qubits} should " "be non-negative.")
-        if isinstance(qubit, List):
-            if len(qubit) != 1:
-                raise QiskitError(f"The experiment if for 1 qubit. For multiple qubits, please use "
-                                  f"parallel experiments.")
-            if qubit[0] < 0:
-                raise QiskitError(f"The index of the qubit {qubit[0]} should " "be non-negative.")
+        if any(
+            self.experiment_options.delays[idx - 1] >= self.experiment_options.delays[idx]
+            for idx in range(1, len(self.experiment_options.delays))
+        ):
+            raise QiskitError(
+                f"The number of identity gates {self.experiment_options.delays} should "
+                "be increasing."
+            )
+
+        if isinstance(self.experiment_options.qubit, list):
+            if len(self.experiment_options.qubit) != 1:
+                raise QiskitError(
+                    "The experiment if for 1 qubit. For multiple qubits,"
+                    " please use parallel experiments."
+                )
+            if self.experiment_options.qubit[0] < 0:
+                raise QiskitError(
+                    f"The index of the qubit {self.experiment_options.qubit[0]} should "
+                    "be non-negative."
+                )
         else:
-            if qubit < 0:
-                raise QiskitError(f"The index of the qubit {qubit} should " "be non-negative.")
+            if self.experiment_options.qubit < 0:
+                raise QiskitError(
+                    f"The index of the qubit {self.experiment_options.qubit} should "
+                    "be non-negative."
+                )
 
-        if n_echos < 1:
-            raise QiskitError(f"The number of echoes {n_echos} should " "be at least 1.")
+        if self.experiment_options.n_echos < 1:
+            raise QiskitError(
+                f"The number of echoes {self.experiment_options.n_echos} should " "be at least 1."
+            )
 
-        if gate_time <= 0:
-            raise QiskitError(f"The gate time {gate_time} should " "be positive.")
+    def circuits(self, backend: Optional[Backend] = None) -> List[QuantumCircuit]:
+        """
+        Args:
+            backend: Optional, a backend object.
 
-    def circuits(self, backend, qubit: Union[List[int], np.array],
-                 n_echos: int = 1,
-                 phase_alt_echo: bool = False):
+        Returns:
+            The experiment circuits.
 
-        conversion_factor = 1
-        #         if self.experiment_options.unit == "dt":
-        #             try:
-        #                 dt_factor = getattr(backend._configuration, "dt")
-        #                 conversion_factor = dt_factor
-        #             except AttributeError as no_dt:
-        #                 raise AttributeError("Dt parameter is missing in backend configuration") from no_dt
-        #         elif self.experiment_options.unit != "s":
-        #             apply_prefix(1, self.experiment_options.unit)
-        #         xdata = 2 * gate_time * np.array(num_of_gates) * n_echos
-        qr = QuantumRegister(max(qubit) + 1)
-        cr = ClassicalRegister(len(qubit))
+        """
+
         circuits = []
-        for circ_index, delay in enumerate(self._delays):
+        qubit = self.experiment_options.qubit
+        for circ_index, delay in enumerate(self.experiment_options.delays):
             circ = QuantumCircuit(max(qubit) + 1, len(qubit))
-            circ.name = 't2circuit_' + str(circ_index) + '_0'
+            circ.name = "t2circuit_" + str(circ_index) + "_0"
             # First Y rotation in 90 degrees
             circ.ry(np.pi / 2, qubit)  # Bring to qubits to X Axis
-            # circ = pad_id_gates(circ, qr, qubit, circ_length)  # ids - waiting
-            #                 circ.delay(delay, qr[qubit], self.experiment_options.unit)
-            circ.delay(delay, qubit, 's')
+            circ.delay(delay, qubit, self.experiment_options.unit)
             circ.rx(np.pi, qubit)
-
-            #                 for echoid in range(n_echos - 1):  # repeat
-            #                     circ = pad_id_gates(circ, qr, qubit, 2 * delay)  # ids
-            #                     if phase_alt_echo and (not echoid % 2):  # optionally
-            #                         circ.x(qr[qubit])  # X
-            #                     else:
-            #                         circ.y(qr[qubit])
-
-            #                 circ.delay(delay, qr[qubit], self.experiment_options.unit)  # ids
-            circ.delay(delay, qubit, 's')
+            circ.delay(delay, qubit, self.experiment_options.unit)
             circ.ry(-np.pi / 2, qubit)  # Y90
-            circ.measure(qubit, 0)  # measure
+            circ.append(Measure(), qubit, [0])  # measure
             circ.metadata = {
                 "experiment_type": self._type,
                 "qubit": self.physical_qubits,
                 "xval": delay,
-                "unit": 's',
+                "unit": self.experiment_options.unit,
             }
-            #             if self.experiment_options.unit == "dt":
-            #                 circ.metadata["dt_factor"] = dt_factor
             circuits.append(circ)
 
         return circuits
