238f9a332852bd7281fbede9167be793e6cf0d1b
Matthew Treinish <mtreinish@kortar.org>
2021-09-22 00:33:56 -0400

    Add missing __init__.py to make qiskit_experiments.test importable (#395)
    This commit fixes a packaging issue with the qiskit_experiments.test
    package. It was missing it's __init__.py file meaning it wasn't a valid
    python package and couldn't be imported when install from a a package.
    This wasn't caught in CI because we have it locally and the path is
    found in the git tree. This commit adds the missing file to fix this
    and adds it to the documentation so there are docs available for the
    packaged test module.
    
    Fixes #393
    
    Co-authored-by: Helena Zhang <Helena.Zhang@ibm.com>

---
+++ qiskit_experiments/test/__init__.py
@@ -0,0 +1,41 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+
+"""
+=================================================================
+Qiskit Experiments Test Utilties (:mod:`qiskit_experiments.test`)
+=================================================================
+
+.. currentmodule:: qiskit_experiments.test
+
+This module contains classes and functions that are used to enable testing
+of Qiskit Experiments. It's primarily composed of fake and mock backends that
+act like a normal :class:`~qiskit.providers.BackendV1` for a real device but
+instead call a simulator internally.
+
+.. autosummary::
+    :toctree: ../stubs/
+
+    MockIQBackend
+    MockRamseyXY
+    MockFineAmp
+    DragBackend
+    T1Backend
+    T2RamseyBackend
+    FakeJob
+
+"""
+
+from .utils import FakeJob
+from .mock_iq_backend import MockIQBackend, MockRamseyXY, MockFineAmp, DragBackend
+from .t1_backend import T1Backend
+from .t2ramsey_backend import T2RamseyBackend
