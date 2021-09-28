7e2ea6cc60a43f5ddf83be2cf8713b0c259d7fa1
Daniel Egger <deg@zurich.ibm.com>
2021-09-28 09:59:08 +0200

    * Small changes to align test_drag

--- test/calibration/experiments/test_drag.py
+++ test/calibration/experiments/test_drag.py
@@ -71,7 +71,7 @@ class TestDragEndToEnd(QiskitTestCase):
         meas_level = exp_data.metadata["job_metadata"][-1]["run_options"]["meas_level"]
 
         self.assertEqual(meas_level, MeasLevel.KERNELED)
-        self.assertTrue(abs(result.value.value - backend.ideal_beta) < test_tol)
+        self.assertTrue(abs(result.value.value - backend.ideal_beta) < self.test_tol)
         self.assertEqual(result.quality, "good")
 
         # Large leakage will make the curves oscillate quickly.
@@ -82,7 +82,7 @@ class TestDragEndToEnd(QiskitTestCase):
         drag.set_experiment_options(betas=np.linspace(-4, 4, 31))
         drag.set_analysis_options(p0={"beta": 1.8, "freq0": 0.08, "freq1": 0.16, "freq2": 0.32})
         drag.set_experiment_options(schedule=self.x_plus)
-        exp_data = drag.run(backend).block_for_results()
+        exp_data = drag.run(backend)
         result = exp_data.analysis_results(1)
 
         meas_level = exp_data.metadata["job_metadata"][-1]["run_options"]["meas_level"]
