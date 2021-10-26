2e99cb779bc3099ef83726f265f70c715a0545e5
Naoki Kanazawa <nkanazawa1989@gmail.com>
2021-10-27 02:56:30 +0900

    Update qiskit_experiments/library/characterization/heat.py
    Co-authored-by: Will Shanks <wshaos@posteo.net>

--- qiskit_experiments/library/characterization/heat.py
+++ qiskit_experiments/library/characterization/heat.py
@@ -383,7 +383,7 @@ class HeatYError(BaseCompositeHeat):
     __analysis_class__ = HeatYAnalysis
 
 
-class HeatZError(BatchExperiment):
+class HeatZError(BatchCompositeHeat):
     """HEAT experiments for Z error amplification.
 
     # section: overview
