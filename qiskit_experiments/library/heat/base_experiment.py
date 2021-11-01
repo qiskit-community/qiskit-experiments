e93aca1a762f60e4b140e75aa2ea1d462edb8f66
Naoki Kanazawa <nkanazawa1989@gmail.com>
2021-11-01 18:28:38 +0900

    Update qiskit_experiments/library/heat/base_experiment.py
    Co-authored-by: Will Shanks <wshaos@posteo.net>

--- qiskit_experiments/library/heat/base_experiment.py
+++ qiskit_experiments/library/heat/base_experiment.py
@@ -10,7 +10,7 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 """
-Base Class for General Hamiltonian Error Amplifying Tomography Experiments.
+Base Class for general Hamiltonian Error Amplifying Tomography experiments.
 """
 
 from abc import ABC, abstractmethod
