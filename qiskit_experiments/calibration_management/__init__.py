34ec72231f5fba251bd185e7c08f099ce6726d1b
Daniel Egger <deg@zurich.ibm.com>
2021-11-03 10:00:07 +0100

    * Removed Drag updater.

--- qiskit_experiments/calibration_management/__init__.py
+++ qiskit_experiments/calibration_management/__init__.py
@@ -39,7 +39,6 @@ Classes
     BackendCalibrations
     Calibrations
     Frequency
-    Drag
 
 Managing Calibration Data
 =========================
@@ -149,4 +148,4 @@ from .calibrations import Calibrations
 from .backend_calibrations import BackendCalibrations
 from .base_calibration_experiment import BaseCalibrationExperiment
 
-from .update_library import Frequency, Drag, FineDragUpdater
+from .update_library import Frequency, FineDragUpdater
