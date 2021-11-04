4c2fd9df65267f4b67b9b959ab65ecff76671afd
Daniel J. Egger <38065505+eggerdj@users.noreply.github.com>
2021-11-04 15:11:42 +0100

    Ramsey refactor. (#485)
    * * Moved RamseyXY to characterization.
    * Added a cal version of the class.
    
    * * refactored RamseyXY.
    
    * * Test config.
    
    * Update qiskit_experiments/calibration_management/base_calibration_experiment.py
    
    Co-authored-by: Will Shanks <wshaos@posteo.net>
    
    * * Arg order in init.
    
    * * Removed updated and fixed tests.
    
    Co-authored-by: Will Shanks <wshaos@posteo.net>

--- qiskit_experiments/library/characterization/__init__.py
+++ qiskit_experiments/library/characterization/__init__.py
@@ -34,6 +34,7 @@ Experiments
     FineAmplitude
     FineXAmplitude
     FineSXAmplitude
+    RamseyXY
     RoughDrag
 
 
@@ -61,4 +62,5 @@ from .cr_hamiltonian_analysis import CrossResonanceHamiltonianAnalysis
 from .rabi import Rabi, EFRabi
 from .half_angle import HalfAngle
 from .fine_amplitude import FineAmplitude, FineXAmplitude, FineSXAmplitude
+from .ramsey_xy import RamseyXY
 from .drag import RoughDrag
