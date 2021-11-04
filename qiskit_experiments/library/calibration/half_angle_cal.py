c472eb364d4c0822bdefca1c4173632a829f8b1f
Daniel J. Egger <38065505+eggerdj@users.noreply.github.com>
2021-11-04 09:30:42 +0100

    Drag cal refactor (#473)
    * * Moved calibration.DragCal -> characterization.RoughDrag.
    * New cal class RoughDragCal.
    * Added test and refactored tests.
    * Adjusted inits.
    
    * * Default options.
    
    * * Black.
    * Amp. update fix.
    
    * * Removed Drag update library test as this is now redundent with RoughDragCal test.
    
    * * Removed Drag updater.
    
    * * Removed unused import.
    
    * * updated NB.
    
    * * Aligned tests to the gate naming.
    
    * * Tutorial NB.
    
    * * Config test.
    
    * * Args order and set options.

--- qiskit_experiments/library/calibration/half_angle_cal.py
+++ qiskit_experiments/library/calibration/half_angle_cal.py
@@ -63,23 +63,6 @@ class HalfAngleCal(BaseCalibrationExperiment, HalfAngle):
 
         self.set_transpile_options(inst_map=calibrations.default_inst_map)
 
-    @classmethod
-    def _default_experiment_options(cls):
-        """Default values for the half angle calibration experiment.
-
-        Experiment Options:
-            result_index (int): The index of the result from which to update the calibrations.
-            group (str): The calibration group to which the parameter belongs. This will default
-                to the value "default".
-
-        """
-        options = super()._default_experiment_options()
-
-        options.result_index = -1
-        options.group = "default"
-
-        return options
-
     def _add_cal_metadata(self, circuits: List[QuantumCircuit]):
         """Add metadata to the circuit to make the experiment data more self contained.
 
