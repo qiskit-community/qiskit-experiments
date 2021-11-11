796d96197fa8f9a1fa41c71bccd1cd62045d70c4
Christopher J. Wood <cjwood@us.ibm.com>
2021-11-11 18:30:03 -0500

    Add ``Settings`` mixin class (#520)
    Co-authored-by: Naoki Kanazawa <nkanazawa1989@gmail.com>

--- qiskit_experiments/framework/composite/batch_experiment.py
+++ qiskit_experiments/framework/composite/batch_experiment.py
@@ -18,11 +18,9 @@ from collections import OrderedDict
 
 from qiskit import QuantumCircuit
 from qiskit.providers.backend import Backend
-from qiskit_experiments.framework.base_experiment import fix_class_docs
 from .composite_experiment import CompositeExperiment, BaseExperiment
 
 
-@fix_class_docs
 class BatchExperiment(CompositeExperiment):
     """Combine multiple experiments into a batch experiment.
 
