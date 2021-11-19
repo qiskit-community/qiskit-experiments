5722020f55cede922736efcbf07c68cf2bfb6f63
Yael Ben-Haim <yaelbh@il.ibm.com>
2021-11-19 16:47:15 +0200

    Bug fix: T1 and T2Ramsey don't run correctly on devices (#529)
    Co-authored-by: Naoki Kanazawa <nkanazawa1989@gmail.com>
    
    Co-authored-by: Naoki Kanazawa <nkanazawa1989@gmail.com>

--- qiskit_experiments/library/characterization/t1.py
+++ qiskit_experiments/library/characterization/t1.py
@@ -127,6 +127,8 @@ class T1(BaseExperiment):
         Raises:
             ValueError: When conversion factor is not set.
         """
+        if self.backend:
+            self._set_backend(self.backend)
         prefactor = self.experiment_options.conversion_factor
 
         if prefactor is None:
