1334ba7ebaeacfc602d74480d42b2ca73942b5ab
Yael Ben-Haim <yaelbh@il.ibm.com>
2022-01-18 13:18:53 +0200

    wrote test_create_experiment

---
+++ test/database_service/test_fake_service.py
@@ -0,0 +1,57 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Test the fake service
+"""
+
+from test.base import QiskitExperimentsTestCase
+from qiskit_experiments.test.fake_service import FakeService
+
+
+class TestFakeService(QiskitExperimentsTestCase):
+    """
+    Test the fake service
+    """
+
+    def setUp(self):
+        super().setUp()
+
+        self.service = FakeService()
+        self.expdict = {}
+
+        num = 0
+        for experiment_type in range(2):
+            for backend_name in range(2):
+                for tags in range(2):
+                    entry = self.generate_entry(experiment_type, backend_name, tags, num)
+                    self.service.create_experiment(**entry)
+                    self.expdict[num] = entry
+                    num += 1
+
+    @staticmethod
+    def generate_entry(experiment_type, backend_name, tags, num):
+        entry = {"experiment_id": str(num), "experiment_type": str(experiment_type), "backend_name": str(backend_name), "tags": ["a"+str(tags), "b"+str(tags)]}
+        if num>2:
+            entry["parent_id"] = str(num%3)
+        return entry
+                        
+    def test_create_experiment(self):
+        self.assertEqual(len(self.service.experiments), 8)
+        num = 0
+        for experiment_type in range(2):
+            for backend_name in range(2):
+                for tags in range(2):
+                    entry = self.generate_entry(experiment_type, backend_name, tags, num)
+                    full_entry = self.service.experiments.loc[lambda df: df["experiment_id"] == entry["experiment_id"], :].to_dict("records")
+                    self.assertEqual(len(full_entry), 1)
+                    self.assertTrue(entry.items() <= full_entry[0].items())
+                    num += 1
