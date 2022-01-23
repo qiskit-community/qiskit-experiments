6593c45fe00bdeb94b0f857d259ff7b24cfecec2
Yael Ben-Haim <yaelbh@il.ibm.com>
2022-01-23 10:45:22 +0200

    test create analysis result

--- test/database_service/test_fake_service.py
+++ test/database_service/test_fake_service.py
@@ -26,28 +26,36 @@ class TestFakeService(QiskitExperimentsTestCase):
         super().setUp()
 
         self.service = FakeService()
+        
         self.expdict = {}
-
-        num = 0
+        expid = 0
         for experiment_type in range(2):
             for backend_name in range(2):
                 for tags in range(2):
-                    entry = self.generate_entry(experiment_type, backend_name, tags, num)
-                    self.service.create_experiment(**entry)
-                    self.expdict[str(num)] = entry
-                    num += 1
+                    expentry = {"experiment_id": str(expid), "experiment_type": str(experiment_type), "backend_name": str(backend_name), "tags": ["a"+str(tags), "b"+str(tags)]}
+                    if expid>2:
+                        expentry["parent_id"] = str(expid%3)
+                    self.service.create_experiment(**expentry)
+                    self.expdict[str(expid)] = expentry
+                    expid += 1
 
-    @staticmethod
-    def generate_entry(experiment_type, backend_name, tags, num):
-        entry = {"experiment_id": str(num), "experiment_type": str(experiment_type), "backend_name": str(backend_name), "tags": ["a"+str(tags), "b"+str(tags)]}
-        if num>2:
-            entry["parent_id"] = str(num%3)
-        return entry
-                        
+        self.resdict = {}
+        resid = 0
+        for experiment_id in [0, 1, 6, 7]:
+            for result_type in range(2):
+                for tags in range(2):
+                    for quality in [True, False]:
+                        for verified in [True, False]:
+                            for result_data in range(2):
+                                resentry = {"experiment_id": str(experiment_id), "result_type": str(result_type), "result_id": str(resid), "tags": ["a"+str(tags), "b"+str(tags)], "quality": quality, "verified": verified, "result_data": {"value": result_data}}
+                                self.service.create_analysis_result(**resentry)
+                                self.resdict[str(resid)] = resentry
+                                resid += 1                                
+                                
     def test_create_experiment(self):
         self.assertEqual(len(self.service.exps), 8)
         is_in_frame = []
-        for i in range(len(self.service.exps)):
+        for i in range(8):
             full_entry = self.service.exps.loc[i, :].to_dict()
             expid = full_entry["experiment_id"]
             self.assertTrue(expid not in is_in_frame)
@@ -60,4 +68,16 @@ class TestFakeService(QiskitExperimentsTestCase):
         for expid in range(8):
             full_entry = self.service.experiment(str(expid))
             entry = self.expdict[str(expid)]
+            self.assertTrue(entry.items() <= full_entry.items())
+
+    def test_create_analysis_result(self):
+        self.assertEqual(len(self.service.results), 128)
+        is_in_frame = []
+        for i in range(128):
+            full_entry = self.service.results.loc[i, :].to_dict()
+            resid = full_entry["result_id"]
+            self.assertTrue(resid not in is_in_frame)
+            is_in_frame.append(resid)
+            self.assertTrue(resid in self.resdict.keys())
+            entry = self.resdict[resid]
             self.assertTrue(entry.items() <= full_entry.items())
