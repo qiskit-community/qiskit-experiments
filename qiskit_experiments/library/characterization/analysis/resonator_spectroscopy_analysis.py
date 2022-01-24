* Units, corresponding test fix, and black.

qiskit_experiments/library/characterization/analysis/resonator_spectroscopy_analysis.py
@@ -57,8 +57,12 @@ class ResonatorSpectroscopyAnalysis(ResonanceAnalysis):
             if len(iqs) > 0:
                 iqs = np.array(iqs)
                 axis.scatter(iqs[:, 0], iqs[:, 1], color="b")
-                axis.set_xlabel("In phase [arb. units]", fontsize=self.options.style.axis_label_size)
-                axis.set_ylabel("Quadrature [arb. units]", fontsize=self.options.style.axis_label_size)
+                axis.set_xlabel(
+                    "In phase [arb. units]", fontsize=self.options.style.axis_label_size
+                )
+                axis.set_ylabel(
+                    "Quadrature [arb. units]", fontsize=self.options.style.axis_label_size
+                )
                 axis.tick_params(labelsize=self.options.style.tick_label_size)
                 axis.grid(True)
 