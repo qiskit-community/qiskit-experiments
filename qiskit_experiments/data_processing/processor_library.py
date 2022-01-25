dac22d9f580a0e3bc32c468d5f2d2f7533754535
catornow <ctornow@student.ethz.ch>
2022-01-25 15:29:36 +0100

    Implemented Daniel's suggestions

--- qiskit_experiments/data_processing/processor_library.py
+++ qiskit_experiments/data_processing/processor_library.py
@@ -34,22 +34,29 @@ def get_processor(
                the interval [0, 1]. The default is True. This option is only relevant if
                kerneled data is used.
              - outcome (string): The measurement outcome that will be passed to a Probability node.
+               The default value is a string of 1's where the length of the string is the number of
+               qubits, e.g. '111' for three qubits.
              - restless_threshold (float): If (repetition delay / T1) is below a given threshold for
-               all physical qubit T1 values the experiment is recognized as a restless experiment and
-               will be analyzed accordingly.
-        index: The index of the job for which to get a data processor.
+               all physical qubit T1 values and active reset is turned off in the experiment run options
+               (init_qubit=False) the experiment is recognized as a restless experiment and will be
+               analyzed accordingly. The default value is 1.
+        index: The index of the job for which to get a data processor. The default value is -1.
 
     Returns:
         An instance of DataProcessor capable of processing the data for the corresponding job.
 
     Notes:
         The following relevant arguments are extracted from the experiment_data metadata run options:
-            - meas_level (MeasLevel): The measurement level of the data to process.
+            - meas_level (MeasLevel): The measurement level of the data to process which is by default
+              set to MeasLevel.CLASSIFIED.
             - meas_return (MeasReturnType): The measurement return (single or avg) of the data to process.
+              The default is MeasReturnType.AVERAGE.
             - init_qubits (bool): If False, the qubits are not reset to the ground state after a measurement.
+              The default is True.
             - physical qubits: The physical qubits used in the experiment.
-            - memory (bool): If True, single-shot measurement bitstrings are returned.
-            - rep_delay (float): The delay between a measurement and the subsequent circuit.
+            - memory (bool): If True, single-shot measurement bitstrings are returned. The default is False.
+            - rep_delay (float): The delay between a measurement and the subsequent circuit. The default is
+              None.
 
         In addition, the following argument is extracted from the experiment_data:
             - t1_values (List[float]): The T1 values of the physical qubits at the time of the experiment.
@@ -68,10 +75,6 @@ def get_processor(
 
     physical_qubits = experiment_data.metadata["physical_qubits"]
     num_qubits = len(physical_qubits)
-    t1_values = [
-        experiment_data.backend.properties().qubit_property(physical_qubit)["T1"][0]
-        for physical_qubit in physical_qubits
-    ]
 
     outcome = analysis_options.get("outcome", "1" * num_qubits)
 
@@ -83,6 +86,10 @@ def get_processor(
     # restless data processing.
     restless = False
     if rep_delay and not init_qubits:
+        t1_values = [
+            experiment_data.backend.properties().qubit_property(physical_qubit)["T1"][0]
+            for physical_qubit in physical_qubits
+        ]
         if [rep_delay / t1_value < restless_threshold for t1_value in t1_values] == [
             True
         ] * num_qubits:
