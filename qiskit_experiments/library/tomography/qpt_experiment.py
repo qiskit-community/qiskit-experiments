e4a275bcfeef14f24cdc27bde53e5b21a348045f
Christopher Wood <cjwood@us.ibm.com>
2022-01-27 15:00:15 -0500

    Avoid storing tomography target in metadata
    Updates State and process tomography experiments to directly set the target option for TomographyAnalysis rather than pass it through experiment data metadata.

--- qiskit_experiments/library/tomography/qpt_experiment.py
+++ qiskit_experiments/library/tomography/qpt_experiment.py
@@ -14,8 +14,11 @@ Quantum Process Tomography experiment
 """
 
 from typing import Union, Optional, Iterable, List, Tuple, Sequence
+import numpy as np
 from qiskit.circuit import QuantumCircuit, Instruction
 from qiskit.quantum_info.operators.base_operator import BaseOperator
+from qiskit.quantum_info import Choi, Operator, Statevector, partial_trace
+from qiskit_experiments.exceptions import QiskitError
 from .tomography_experiment import TomographyExperiment
 from .qpt_analysis import ProcessTomographyAnalysis
 from . import basis
@@ -88,5 +91,50 @@ class ProcessTomography(TomographyExperiment):
             preparation_qubits=preparation_qubits,
             basis_indices=basis_indices,
             qubits=qubits,
+            analysis=ProcessTomographyAnalysis(),
         )
-        self.analysis = ProcessTomographyAnalysis()
+
+        # Set target quantum channel
+        self.analysis.set_options(target=self._target_quantum_channel())
+
+    def _target_quantum_channel(self) -> Union[Choi, Operator]:
+        """Return the process tomography target"""
+        # Check if circuit contains measure instructions
+        # If so we cannot return target state
+        circuit_ops = self._circuit.count_ops()
+        if "measure" in circuit_ops:
+            return None
+        perm_circ = self._permute_circuit()
+        try:
+            if "reset" in circuit_ops or "kraus" in circuit_ops or "superop" in circuit_ops:
+                channel = Choi(perm_circ)
+            else:
+                channel = Operator(perm_circ)
+        except QiskitError:
+            # Circuit couldn't be simulated
+            return None
+
+        total_qubits = self._circuit.num_qubits
+        if self._meas_qubits:
+            num_meas = len(self._meas_qubits)
+        else:
+            num_meas = total_qubits
+        if self._prep_qubits:
+            num_prep = len(self._prep_qubits)
+        else:
+            num_prep = total_qubits
+
+        if num_prep == total_qubits and num_meas == total_qubits:
+            return channel
+
+        # Trace out non-measurement subsystems
+        tr_qargs = []
+        if self._prep_qubits:
+            tr_qargs += list(range(num_prep, total_qubits))
+        if self._meas_qubits:
+            tr_qargs += list(range(total_qubits + num_meas, 2 * total_qubits))
+
+        chan_state = Statevector(np.ravel(channel, order="F"))
+        chan_state = partial_trace(chan_state, tr_qargs) / 2 ** (total_qubits - num_meas)
+        channel = Choi(chan_state.data, input_dims=[2] * num_prep, output_dims=[2] * num_meas)
+        return channel
