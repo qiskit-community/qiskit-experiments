3d006a29c3d4a59ed5f8aa0865d9c5eb989f9890
knzwnao <nkanazawa1989@gmail.com>
2022-02-03 05:34:27 +0900

    fix dataclass initialization bug

--- qiskit_experiments/curve_analysis/visualization/style.py
+++ qiskit_experiments/curve_analysis/visualization/style.py
@@ -40,4 +40,6 @@ class PlotterStyle:
 
     # sigma values for confidence interval, which are the tuple of (sigma, alpha).
     # the alpha indicates the transparency of the corresponding interval plot.
-    plot_sigma: List[Tuple[float, float]] = dataclasses.field(default=[(1.0, 0.3), (3.0, 0.1)])
+    plot_sigma: List[Tuple[float, float]] = dataclasses.field(
+        default_factory=lambda: [(1.0, 0.3), (3.0, 0.1)]
+    )
