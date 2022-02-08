f7d10ad16f3968e9b1363c5970630502288524ee
merav-aharoni <46567124+merav-aharoni@users.noreply.github.com>
2022-02-08 12:42:13 +0200

    T phi experiment (#355)
    * Beginning implementation of T_phi
    
    * Added init files
    
    * Built circuits for T_phi
    
    * Defined class T_phi
    
    * Added run method to class Tphi
    
    * First stage of Tphi - up to run
    
    * Added test for Tphi. Added backend for testing, based on T1 and T2Ramsey backends.
    
    * black
    
    * Fixed return FakeJob from TphiBackend.run
    
    * Get sub-experiment analysis results from expdata.child_data
    
    * Added _run_analysis for TphiAnalysis. Added  parameter to BatchExperiment and to CompositeExperiment. Fix in return value of CompositeAnalysis._run_analysis.
    
    * Cleaned up return value for Tphi analysis results. Computed the final results. Added tests for Tphi
    
    * Removed usage of 'units' in Tphi. Added usage of set_experiment_options instead of plain parameters for setting delays_t1 and delays_t2 in Tphi.
    
    * Removed unnecessary variable 'unit' and unnecessary imports.
    
    * Added missing 'TphiAnalysis in __init__
    
    * Added documentation for Tphi
    
    * Added 'analysis' parameter to BatchExperiment and to CompositeExperiment
    
    * Added title and section to tphi tutorial
    
    * Fixed Tphi computation. Wrote tutorial. Added computation with uncertainties.
    
    * Fixed title of tutorial
    
    * Removed unnecessary prints and empty lines
    
    * Change of parameter merged from main
    
    * Added references for Tphi
    
    * Updated copyright to 2022. Added _set_backend to Tphi
    
    * Bug fix: parameter 'backend' was missing in calls to T1 and T2Ramsey
    
    * Cleaned up Tphi tutorial
    
    * Removed dataProcessor from default_options because it was causing a warning when loading from a device
    
    * Added release notes for tphi
    
    * Syntax changes resulting from code review
    
    * Update qiskit_experiments/library/characterization/tphi.py
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>
    
    * Update qiskit_experiments/library/characterization/tphi.py
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>
    
    * Removed two unnecessary methods, and did some cleaning following suggestions from review
    
    * Added class documentation. Removed overriding of shots
    
    * Removed unnecessary parameter  that I previously added to BatchExperiment and CompositeExperiment constructors
    
    * Removed setting of shots in backend options. Changed 'block_for_results' to 'assertExperimentDone'
    
    * Fixed setting of default options in Tphi
    
    * Added links to classes in the release notes
    
    * Added info on the tphi experiment to the release notes
    
    * Implemented a set_experiment_options for tphi, that sets the options for the sub-experiments, without storing the options for the main object. Removed _default_experiment_options, that is no longer needed
    
    * Added test to check that circuits are modified correctly when using set_experiment_options after run
    
    * Added component analysis classes to the constructor of TphiAnalysis() following changes in PR 633
    
    * Added setting of osc_freq to set_experiment_options. Added a respective test
    
    * Added to tests 'assertExperimentDone'
    
    Co-authored-by: Yael Ben-Haim <yaelbh@il.ibm.com>

---
+++ test/test_tphi.py
@@ -0,0 +1,104 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021, 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Test T2Ramsey experiment
+"""
+
+from test.base import QiskitExperimentsTestCase
+from qiskit_experiments.library import Tphi
+from qiskit_experiments.test.tphi_backend import TphiBackend
+from qiskit_experiments.library.characterization.analysis.t1_analysis import T1Analysis
+from qiskit_experiments.library.characterization.analysis.t2ramsey_analysis import T2RamseyAnalysis
+from qiskit_experiments.library.characterization.analysis.tphi_analysis import TphiAnalysis
+
+
+class TestTphi(QiskitExperimentsTestCase):
+    """Test Tphi experiment"""
+
+    __tolerance__ = 0.1
+
+    def test_tphi_end_to_end(self):
+        """
+        Run a complete Tphi experiment on a fake Tphi backend
+        """
+        delays_t1 = list(range(1, 40, 3))
+        delays_t2 = list(range(1, 51, 2))
+        exp = Tphi(qubit=0, delays_t1=delays_t1, delays_t2=delays_t2, osc_freq=0.1)
+
+        t1 = 20
+        t2ramsey = 25
+        backend = TphiBackend(t1=t1, t2ramsey=t2ramsey, freq=0.1)
+        analysis = TphiAnalysis([T1Analysis(), T2RamseyAnalysis()])
+        expdata = exp.run(backend=backend, analysis=analysis)
+        self.assertExperimentDone(expdata)
+        result = expdata.analysis_results("T_phi")
+        estimated_tphi = 1 / ((1 / t2ramsey) - (1 / (2 * t1)))
+        self.assertAlmostEqual(
+            result.value.value,
+            estimated_tphi,
+            delta=TestTphi.__tolerance__ * result.value.value,
+        )
+        self.assertEqual(result.quality, "good", "Result quality bad")
+
+    def test_tphi_with_changing_params(self):
+        """
+        Run Tphi experiment, then set new delay values in set_experiment_options, and check
+        that the new experiment has the correct delay values.
+        """
+        delays_t1 = list(range(1, 40, 3))
+        delays_t2 = list(range(1, 50, 2))
+        exp = Tphi(qubit=0, delays_t1=delays_t1, delays_t2=delays_t2, osc_freq=0.1)
+
+        t1 = 20
+        t2ramsey = 25
+        backend = TphiBackend(t1=t1, t2ramsey=t2ramsey, freq=0.1)
+        analysis = TphiAnalysis([T1Analysis(), T2RamseyAnalysis()])
+        expdata = exp.run(backend=backend, analysis=analysis)
+        self.assertExperimentDone(expdata)
+
+        data_t1 = expdata.child_data(0).data()
+        x_values_t1 = [datum["metadata"]["xval"] for datum in data_t1]
+        data_t2 = expdata.child_data(1).data()
+        x_values_t2 = [datum["metadata"]["xval"] for datum in data_t2]
+        self.assertListEqual(x_values_t1, delays_t1, "Incorrect delays_t1")
+        self.assertListEqual(x_values_t2, delays_t2, "Incorrect delays_t2")
+
+        new_delays_t1 = list(range(1, 45, 3))
+        new_delays_t2 = list(range(1, 55, 2))
+        new_osc_freq = 0.2
+
+        exp.set_experiment_options(
+            delays_t1=new_delays_t1, delays_t2=new_delays_t2, osc_freq=new_osc_freq
+        )
+        expdata = exp.run(backend=backend, analysis=analysis)
+        self.assertExperimentDone(expdata)
+
+        data_t1 = expdata.child_data(0).data()
+        x_values_t1 = [datum["metadata"]["xval"] for datum in data_t1]
+        data_t2 = expdata.child_data(1).data()
+        x_values_t2 = [datum["metadata"]["xval"] for datum in data_t2]
+        self.assertListEqual(x_values_t1, new_delays_t1, "Option delays_t1 not set correctly")
+        self.assertListEqual(x_values_t2, new_delays_t2, "Option delays_t2 not set correctly")
+        new_freq_t2 = data_t2[0]["metadata"]["osc_freq"]
+        self.assertEqual(new_freq_t2, new_osc_freq, "Option osc_freq not set correctly")
+
+    def test_roundtrip_serializable(self):
+        """Test round trip JSON serialization"""
+        exp = Tphi(0, [1], [2], 3)
+        self.assertRoundTripSerializable(exp, self.json_equiv)
+
+    def test_analysis_config(self):
+        """Test converting analysis to and from config works"""
+        analysis = TphiAnalysis([T1Analysis(), T2RamseyAnalysis()])
+        loaded_analysis = analysis.from_config(analysis.config())
+        self.assertNotEqual(analysis, loaded_analysis)
+        self.assertEqual(analysis.config(), loaded_analysis.config())
