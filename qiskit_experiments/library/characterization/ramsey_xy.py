4d2f5793a248fff2e4443b3d61a6dd897e7a4d4d
Christopher J. Wood <cjwood@us.ibm.com>
2022-04-04 13:57:11 -0400

    Remove job metadata from BaseExperiment (#719)
    * Remove _additional_metadata
    
    * Update CurveAnalysis experiments to store run_options
    
    This updates curve analysis experiments to explicitly store data processor run options in metadata rather than using the job_metadata field
    
    * Remove _add_job_metadata method
    
    * Remove run opts from Base metadata
    
    This moves the measurement level run option metadata needed by curve analysis base experiments into the individual experiments.

--- qiskit_experiments/library/characterization/ramsey_xy.py
+++ qiskit_experiments/library/characterization/ramsey_xy.py
@@ -231,3 +231,12 @@ class RamseyXY(BaseExperiment, RestlessMixin):
             circs.extend([assigned_x, assigned_y])
 
         return circs
+
+    def _metadata(self):
+        metadata = super()._metadata()
+        # Store measurement level and meas return if they have been
+        # set for the experiment
+        for run_opt in ["meas_level", "meas_return"]:
+            if hasattr(self.run_options, run_opt):
+                metadata[run_opt] = getattr(self.run_options, run_opt)
+        return metadata
