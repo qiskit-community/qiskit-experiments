a26f5b1292976c96cf367691441ac8625cc82915
Daniel Egger <egger.d.j@gmail.com>
2022-04-27 13:50:37 +0200

    * Test fix.

--- test/data_processing/test_data_processing.py
+++ test/data_processing/test_data_processing.py
@@ -839,5 +839,5 @@ class TestAvgDataAndSVD(BaseDataProcessorTest):
         processed = processor(self.data.data())
         np.testing.assert_array_almost_equal(
             unp.nominal_values(processed),
-            np.array([[0.0, 1.0], [1.0, 0.0], [0.5, 0.5], [0.75, 0.25]]),
+            np.array([[1.0, 0.0], [0.0, 1.0], [0.5, 0.5], [0.25, 0.75]]),
         )
