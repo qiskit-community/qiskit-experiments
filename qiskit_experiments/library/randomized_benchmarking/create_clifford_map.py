b05f7dfe2133be9f4fd112ab01ba502c881e3895
Merav Aharoni <merav@il.ibm.com>
2022-07-20 22:45:40 +0300

    black, lint and documentation

--- qiskit_experiments/library/randomized_benchmarking/create_clifford_map.py
+++ qiskit_experiments/library/randomized_benchmarking/create_clifford_map.py
@@ -11,14 +11,37 @@
 # that they have been altered from the originals.
 """
 This is a script used to create the data in clifford_data.py.
+Every Clifford is represented by a number. We store a list of the compositions of Cliffords represented
+as numbers. For example, if Clifford1.compose(Clifford2) == Clifford3, then we conceptually,
+we store {(1, 2) : 3}. we don't actually store the map, but only the results of the compose in an array.
+This is more efficient in performance. The result is found using the indices of the input Cliffords.
+Similarly, we store for each number representing a Clifford, the number representing the
+inverse Clifford.
+For compose, we don't actually store the full compose table of all-cliffords X all-cliffords.
+Instead, we define an array of single-gate-cliffords. This comprises all Cliffords that consist
+of a single gate. There are 8 such Cliffords for 1-qubit, and 20 such Cliffords for 2-qubits.
+It is sufficient to store the compose table of all-cliffords X single-gate-cliffords,
+since for every Clifford on the right hand side, we can break it down into single gate Cliffords,
+and do the composition one at a time. This greatly reduces the storage space for the array of
+composition results (from O(n^2) to O(n)), where n is the number of Cliffords.
 """
 import itertools
 
-from qiskit.circuit import QuantumCircuit, Gate
-from qiskit_experiments.library.randomized_benchmarking.clifford_utils import CliffordUtils
-from qiskit.circuit.library import SXGate, CXGate, IGate, HGate, SXdgGate, SGate, \
-    XGate, SXGate, YGate, ZGate, SdgGate, CXGate
+from qiskit.circuit import QuantumCircuit
+from qiskit.circuit.library import (
+    IGate,
+    HGate,
+    SXdgGate,
+    SGate,
+    XGate,
+    SXGate,
+    YGate,
+    ZGate,
+    SdgGate,
+    CXGate,
+)
 from qiskit.quantum_info.operators.symplectic import Clifford
+from qiskit_experiments.library.randomized_benchmarking.clifford_utils import CliffordUtils
 
 gate_list_1q = [
     IGate(),
@@ -29,15 +52,21 @@ gate_list_1q = [
     SXGate(),
     YGate(),
     ZGate(),
-    SdgGate()
+    SdgGate(),
 ]
 num_to_cliff_1q = {}
 cliff_to_num_1q = {}
 num_to_cliff_2q = {}
 cliff_to_num_2q = {}
 
-def generate_nums_for_single_gate_cliffs_1q():
-    # We generate an array mapping numbers to Cliffords and the reverse array.
+
+def gen_nums_single_gate_cliffs_1q():
+    """
+    We generate an array mapping numbers to Cliffords and the reverse array.
+    Based on this array, we build a mapping from every single-gate-clifford to its number.
+    The mapping actually looks like {(gate, '[0]'): num}, where [0] represents qubit 0.
+    The qubit is added to be consistent with the format for 2 qubits.
+    """
     for i in range(24):
         cliff = CliffordUtils.clifford_1_qubit(i)
         num_to_cliff_1q[i] = cliff
@@ -48,20 +77,24 @@ def generate_nums_for_single_gate_cliffs_1q():
         qc = QuantumCircuit(1)
         qc.append(gate, [0])
         cliff = Clifford(qc)
-        repr = Clifford(cliff)
-        if repr.__repr__() in cliff_to_num_1q.keys():
-            num = cliff_to_num_1q[repr.__repr__()]
+        if cliff.__repr__() in cliff_to_num_1q.keys():
+            num = cliff_to_num_1q[cliff.__repr__()]
             # qubit_as_str is not really necessary. It is only added to be consistent
             # with the representation for 2 qubits
-            qubit_as_str = '[0]'
+            qubit_as_str = "[0]"
             clifford_single_gate_to_num[(gate.name, qubit_as_str)] = num
         else:
             print("not found")
     file.write(f"CLIFF_SINGLE_GATE_MAP_1Q = {clifford_single_gate_to_num}\n")
     return clifford_single_gate_to_num
 
-def generate_nums_for_single_gate_cliffs_2q():
-    # We generate an array mapping numbers to Cliffords and the reverse array.
+
+def gen_nums_single_gate_cliffs_2q():
+    """
+    We generate an array mapping numbers to Cliffords and the reverse array.
+    Based on this array, we build a mapping from every single-gate-clifford to its number.
+    The mapping actually looks like {(gate, '[qubits]'): num}.
+    """
     for i in range(CliffordUtils.NUM_CLIFFORD_2_QUBIT):
         cliff = CliffordUtils.clifford_2_qubit(i)
         num_to_cliff_2q[i] = cliff
@@ -72,12 +105,11 @@ def generate_nums_for_single_gate_cliffs_2q():
         qc = QuantumCircuit(2)
         qc.append(gate, [qubit])
         cliff = Clifford(qc)
-        repr = Clifford(cliff)
-        if repr.__repr__() in cliff_to_num_2q:
-            num = cliff_to_num_2q[repr.__repr__()]
+        if cliff.__repr__() in cliff_to_num_2q:
+            num = cliff_to_num_2q[cliff.__repr__()]
             # qubit_as_str is not really necessary. It is only added to be consistent
             # with the representation for 2 qubits
-            qubit_as_str = '['+ str(qubit) +']'
+            qubit_as_str = "[" + str(qubit) + "]"
             clifford_single_gate_to_num[(gate.name, qubit_as_str)] = num
         else:
             print("not found")
@@ -86,18 +118,19 @@ def generate_nums_for_single_gate_cliffs_2q():
         qc = QuantumCircuit(2)
         qc.append(CXGate(), qubits)
         cliff = Clifford(qc)
-        repr = Clifford(cliff)
-        if repr.__repr__() in cliff_to_num_2q.keys():
-            num = cliff_to_num_2q[repr.__repr__()]
+        if cliff.__repr__() in cliff_to_num_2q.keys():
+            num = cliff_to_num_2q[cliff.__repr__()]
         else:
             print("not found")
-        direction = "[0, 1]" if qubits == [0,1] else "[1, 0]"
+        direction = "[0, 1]" if qubits == [0, 1] else "[1, 0]"
         clifford_single_gate_to_num[("cx", direction)] = num
     file.write(f"CLIFF_SINGLE_GATE_MAP_2Q = {clifford_single_gate_to_num}\n")
     return clifford_single_gate_to_num
 
+
 def create_compose_map_1q():
-    """Creates the data in CLIFF_COMPOSE_DATA and CLIFF_INVERSE_DATA"""
+    """Creates the data in compose data in CLIFF_COMPOSE_DATA and
+    the inverse data in CLIFF_INVERSE_DATA"""
     products = {}
     for i in range(CliffordUtils.NUM_CLIFFORD_1_QUBIT):
         cliff1 = num_to_cliff_1q[i]
@@ -123,6 +156,7 @@ def create_compose_map_1q():
         file.write(f"{invs[i]},")
     file.write("]")
     file.write("\n")
+
 
 def create_compose_map_2q():
     """Creates the data in CLIFF_COMPOSE_DATA and CLIFF_INVERSE_DATA"""
@@ -152,8 +186,9 @@ def create_compose_map_2q():
     file.write("]")
     file.write("\n")
 
+
 with open("clifford_data.py", "w") as file:
-    single_gate_clifford_map_1q = generate_nums_for_single_gate_cliffs_1q()
-    single_gate_clifford_map_2q = generate_nums_for_single_gate_cliffs_2q()
+    single_gate_clifford_map_1q = gen_nums_single_gate_cliffs_1q()
+    single_gate_clifford_map_2q = gen_nums_single_gate_cliffs_2q()
     create_compose_map_1q()
     create_compose_map_2q()
--- qiskit_experiments/library/randomized_benchmarking/generate_transpiled_circuits.py
+++ qiskit_experiments/library/randomized_benchmarking/generate_transpiled_circuits.py
@@ -1,29 +1,55 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+This file generates all Cliffords after transpilation. The user can select
+the basis set to use for transpilation. The Cliffords are stored in a .qpy file.
+"""
 from typing import List
 from qiskit import qpy
 from qiskit_experiments.library.randomized_benchmarking import CliffordUtils
 
-#basis_gates=["rz", "sx", "cx"]
-basis_gates=["x", "h", "s", "cx"]
+basis_gates_to_generate = ["rz", "sx", "cx"]
+# basis_gates_to_generate = ["x", "h", "s", "cx"]
 
-def generate_1q_transpiled_clifford_circuits(basis_gates: List[str]):
-    """Generate all transpiled clifford circuits"""
+
+def generate_1q_transpiled_cliffs(basis_gates: List[str]):
+    """Generate all 1-qubit transpiled clifford circuits"""
     transpiled_circs = []
     for num in range(0, CliffordUtils.NUM_CLIFFORD_1_QUBIT):
         circ = CliffordUtils.clifford_1_qubit_circuit(num=num)
         transpiled_circ = CliffordUtils.transpile_single_clifford(circ, basis_gates)
         transpiled_circs.append(transpiled_circ)
-    with open('transpiled_circs_1q_x_h_s.qpy', 'wb') as fd:
+    suffix = ""
+    for n in basis_gates_to_generate[0:-1]:
+        suffix += "_" + n
+    file_name = "transpiled_circs_1q" + suffix + ".qpy"
+    with open(file_name, "wb") as fd:
         qpy.dump(transpiled_circs, fd)
 
-def generate_2q_transpiled_clifford_circuits(basis_gates: List[str]):
-    """Generate all transpiled clifford circuits"""
+
+def generate_2q_transpiled_cliffs(basis_gates: List[str]):
+    """Generate all 2-qubit transpiled clifford circuits"""
     transpiled_circs = []
     for num in range(0, CliffordUtils.NUM_CLIFFORD_2_QUBIT):
         circ = CliffordUtils.clifford_2_qubit_circuit(num=num)
         transpiled_circ = CliffordUtils.transpile_single_clifford(circ, basis_gates)
         transpiled_circs.append(transpiled_circ)
-    with open('transpiled_circs_2q_x_h_s_cx.qpy', 'wb') as fd:
+    suffix = ""
+    for n in basis_gates_to_generate:
+        suffix += "_" + n
+    file_name = "transpiled_circs_2q" + suffix + ".qpy"
+    with open(file_name, "wb") as fd:
         qpy.dump(transpiled_circs, fd)
 
-generate_1q_transpiled_clifford_circuits(basis_gates=basis_gates)
-generate_2q_transpiled_clifford_circuits(basis_gates=basis_gates)
+
+generate_1q_transpiled_cliffs(basis_gates=basis_gates_to_generate)
+generate_2q_transpiled_cliffs(basis_gates=basis_gates_to_generate)
