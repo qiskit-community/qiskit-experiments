849e990654698f5d73b44f63757623d9edf3bc19
Christopher Wood <cjwood@us.ibm.com>
2022-07-26 12:05:37 -0400

    Deprecate scipy tomo fitters

--- qiskit_experiments/library/tomography/fitters/scipy_lstsq.py
+++ qiskit_experiments/library/tomography/fitters/scipy_lstsq.py
@@ -16,7 +16,7 @@ Linear least-square MLE tomography fitter.
 from typing import Optional, Dict, Tuple
 import numpy as np
 import scipy.linalg as la
-
+from qiskit.utils import deprecate_function
 from qiskit_experiments.exceptions import AnalysisError
 from qiskit_experiments.library.tomography.basis import (
     MeasurementBasis,
@@ -24,7 +24,15 @@ from qiskit_experiments.library.tomography.basis import (
 )
 from . import lstsq_utils
 
+# Note this warning doesnt show up when run in analysis so we
+# also add a warning when setting the option value that calls this function
 
+# pylint: disable = bad-docstring-quotes
+@deprecate_function(
+    "The scipy lstsq tomography fitters have been deprecated and will "
+    "be removed in the 0.5.0 release. Use the `linear_lstsq`, "
+    "`cvxpy_linear_lstsq`, or `cvxpy_gaussian_lstsq` fitter instead."
+)
 def scipy_linear_lstsq(
     outcome_data: np.ndarray,
     shot_data: np.ndarray,
