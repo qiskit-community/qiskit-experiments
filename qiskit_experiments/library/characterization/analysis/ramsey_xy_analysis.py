51750a5a53950b760bd257ddbfc1c4fa12d66d75
Naoki Kanazawa <nkanazawa1989@gmail.com>
2022-08-10 18:52:06 +0900

    More robust RamseyXY fit. - Fix wrong tau estimation (value was inverted) - Add case of low frequency - Add multiple guesses for FFT frequency uncertainty

--- qiskit_experiments/library/characterization/analysis/ramsey_xy_analysis.py
+++ qiskit_experiments/library/characterization/analysis/ramsey_xy_analysis.py
@@ -37,28 +37,28 @@ class RamseyXYAnalysis(curve.CurveAnalysis):
         defpar \rm amp:
             desc: Amplitude of both series.
             init_guess: Half of the maximum y value less the minimum y value.
-            bounds: [-2, 2] scaled to the maximum signal value.
+            bounds: [0, maximum absolute Y data]
 
         defpar \tau:
             desc: The exponential decay of the curve.
             init_guess: The initial guess is obtained by fitting an exponential to the
                 square root of (X data)**2 + (Y data)**2.
-            bounds: [0, inf].
+            bounds: [0, inf]
 
         defpar \rm base:
             desc: Base line of both series.
             init_guess: Roughly the average of the data.
-            bounds: [minimum Y data, maximum Y data]
+            bounds: [0, maximum absolute Y data]
 
         defpar \rm freq:
             desc: Frequency of both series. This is the parameter of interest.
             init_guess: The frequency with the highest power spectral density.
-            bounds: [-inf, inf].
+            bounds: [-inf, inf]
 
         defpar \rm phase:
             desc: Common phase offset.
             init_guess: 0
-            bounds: [-pi, pi].
+            bounds: [-pi, pi]
     """
 
     def __init__(self):
@@ -108,45 +108,77 @@ class RamseyXYAnalysis(curve.CurveAnalysis):
         Returns:
             List of fit options that are passed to the fitter function.
         """
-        y_max = np.max(curve_data.y)
-        y_min = np.min(curve_data.y)
-        y_ptp = y_max - y_min
+        ramx_data = curve_data.get_subset_of("X")
+        ramy_data = curve_data.get_subset_of("Y")
 
         user_opt.bounds.set_if_empty(
-            amp=(0, y_ptp),
+            amp=(0, np.inf),
             tau=(0, np.inf),
-            base=(y_min, y_max),
+            base=(0, np.inf),
             phase=(-np.pi, np.pi),
         )
 
-        # Default guess values
-        freq_guesses, base_guesses = [], []
-        for series in ["X", "Y"]:
-            data = curve_data.get_subset_of(series)
-            freq_guesses.append(curve.guess.frequency(data.x, data.y))
-            base_guesses.append(curve.guess.constant_sinusoidal_offset(data.y))
+        # At very low frequency, y value of X (Y) curve stay at P=1.0 (0.5) for all x values.
+        # Computing y peak-to-peak with combined data gives fake amplitude of 0.25.
+        # Same for base, i.e. P=0.75 is often estimated in this case.
+        avg_y_ptp = 0.5 * (np.ptp(ramx_data.y) + np.ptp(ramy_data.y))
+        max_y = np.max(np.abs(curve_data.y))
 
-        freq_val = float(np.average(freq_guesses))
-        user_opt.p0.set_if_empty(base=np.average(base_guesses))
+        if avg_y_ptp < 0.1 * max_y:
+            # When peak-to-peak value is less than 10% of full data range,
+            # likely frequency is too low to estimate.
+            # In this case, Ramsey Y curve corresponds to P=0.5, i.e. base.
+            avg_x = np.average(ramx_data.y)
+            avg_y = np.average(ramy_data.y)
+            user_opt.p0.set_if_empty(
+                amp=avg_x - avg_y,
+                tau=100 * np.max(curve_data.x),
+                base=avg_y,
+                phase=0.0,
+                freq=0.0,
+            )
+            return user_opt
 
-        # Guess the exponential decay by combining both curves
-        data_x = curve_data.get_subset_of("X")
-        data_y = curve_data.get_subset_of("Y")
-        decay_data = (data_x.y - user_opt.p0["base"]) ** 2 + (data_y.y - user_opt.p0["base"]) ** 2
-
+        base_guess = curve.guess.constant_sinusoidal_offset(curve_data.y)
         user_opt.p0.set_if_empty(
-            tau=-curve.guess.exp_decay(data_x.x, decay_data),
-            amp=0.5 * y_ptp,
+            amp=0.5 * avg_y_ptp,
+            base=base_guess,
             phase=0.0,
         )
 
-        opt_fp = user_opt.copy()
-        opt_fp.p0.set_if_empty(freq=freq_val)
+        # Guess the exponential decay by combining both curves
+        decay_data = (ramx_data.y - user_opt.p0["base"]) ** 2 + (
+            ramy_data.y - user_opt.p0["base"]
+        ) ** 2
+        if decay_data[0] - decay_data[-1] < 0.95 * 0.5 * avg_y_ptp:
+            # When decay is less than 95 % of peak-to-peak value, ignore decay and
+            # set large enough tau value compared with the measured x range.
+            user_opt.p0.set_if_empty(tau=1000 * np.max(curve_data.x))
+        else:
+            user_opt.p0.set_if_empty(tau=-1 / curve.guess.exp_decay(ramx_data.x, decay_data))
 
-        opt_fm = user_opt.copy()
-        opt_fm.p0.set_if_empty(freq=-freq_val)
+        # Guess the oscillation frequency
+        xyfreq_guesses = []
+        for series in (ramx_data, ramy_data):
+            # Remove offset to eliminate DC peak
+            xyfreq_guesses.append(curve.guess.frequency(series.x, series.y - base_guess))
+        freq_val = float(np.average(xyfreq_guesses))
 
-        return [opt_fp, opt_fm]
+        # FFT might be up to 1/2 bin off
+        df = 2 * np.pi / (np.min(np.diff(ramx_data.x)) * ramx_data.x.size)
+        freq_guesses = [freq_val - df, freq_val + df, freq_val]
+
+        # Ramsey XY is frequency sign sensitive.
+        # Since experimental data is noisy, correct sign is hardly estimated with phase velocity.
+        # Try both positive and negative frequency to find the best fit.
+        opts = []
+        for sign in (1, -1):
+            for freq_guess in freq_guesses:
+                opt = user_opt.copy()
+                opt.p0.set_if_empty(freq=sign * freq_guess)
+                opts.append(opt)
+
+        return opts
 
     def _evaluate_quality(self, fit_data: curve.CurveFitResult) -> Union[str, None]:
         """Algorithmic criteria for whether the fit is good or bad.
