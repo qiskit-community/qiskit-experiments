fc22ff145a290fce2e100e60afa40a382d0b2af9
Conrad Haupt <conrad.haupt@ibm.com>
2022-09-12 10:23:34 +0200

    Move drawers to .visualization.drawers submodule

--- qiskit_experiments/visualization/__init__.py
+++ qiskit_experiments/visualization/__init__.py
@@ -63,8 +63,7 @@
 
 from enum import Enum
 
-from .base_drawer import BaseCurveDrawer
-from .mpl_drawer import MplCurveDrawer
+from .drawers import BaseCurveDrawer, MplCurveDrawer
 
 from . import fit_result_plotters
 from .curves import plot_scatter, plot_errorbar, plot_curve_fit
