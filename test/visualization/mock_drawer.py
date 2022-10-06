cd6f92a8a49b7a1fba5dde8df8d6a8d9992dc196
Conrad Haupt <conrad@conradhaupt.com>
2022-10-06 13:33:06 +0200

    Add new plotting module for experiment and analysis figures (#902)
    * The visualization code has been moved to its own module. It now also implements a bridge interface 
    where a plotter determines what data is plotted and how while a drawer implements a plotting backend
    (such as matplotlib).
    
    Co-authored-by: Daniel J. Egger <38065505+eggerdj@users.noreply.github.com>
    Co-authored-by: Naoki Kanazawa <nkanazawa1989@gmail.com>

---
+++ test/visualization/mock_drawer.py
@@ -0,0 +1,110 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Mock drawer for testing.
+"""
+
+from typing import Optional, Sequence, Tuple
+
+from qiskit_experiments.visualization import BaseDrawer, PlotStyle
+
+
+class MockDrawer(BaseDrawer):
+    """Mock drawer for visualization tests.
+
+    Most methods of this class do nothing.
+    """
+
+    @property
+    def figure(self):
+        """Does nothing."""
+        pass
+
+    @classmethod
+    def _default_style(cls) -> PlotStyle:
+        """Default style.
+
+        Style Param:
+            overwrite_param: A test style parameter to be overwritten by a test.
+        """
+        style = super()._default_style()
+        style["overwrite_param"] = "overwrite_param"
+        return style
+
+    def initialize_canvas(self):
+        """Does nothing."""
+        pass
+
+    def format_canvas(self):
+        """Does nothing."""
+        pass
+
+    def scatter(
+        self,
+        x_data: Sequence[float],
+        y_data: Sequence[float],
+        x_err: Optional[Sequence[float]] = None,
+        y_err: Optional[Sequence[float]] = None,
+        name: Optional[str] = None,
+        label: Optional[str] = None,
+        legend: bool = False,
+        **options,
+    ):
+        """Does nothing."""
+        pass
+
+    def line(
+        self,
+        x_data: Sequence[float],
+        y_data: Sequence[float],
+        name: Optional[str] = None,
+        label: Optional[str] = None,
+        legend: bool = False,
+        **options,
+    ):
+        """Does nothing."""
+        pass
+
+    def filled_y_area(
+        self,
+        x_data: Sequence[float],
+        y_ub: Sequence[float],
+        y_lb: Sequence[float],
+        name: Optional[str] = None,
+        label: Optional[str] = None,
+        legend: bool = False,
+        **options,
+    ):
+        """Does nothing."""
+        pass
+
+    def filled_x_area(
+        self,
+        x_ub: Sequence[float],
+        x_lb: Sequence[float],
+        y_data: Sequence[float],
+        name: Optional[str] = None,
+        label: Optional[str] = None,
+        legend: bool = False,
+        **options,
+    ):
+        """Does nothing."""
+        pass
+
+    def textbox(
+        self,
+        description: str,
+        rel_pos: Optional[Tuple[float, float]] = None,
+        **options,
+    ):
+        """Does nothing."""
+        pass
---
+++ test/visualization/test_plotter_mpldrawer.py
@@ -0,0 +1,40 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""
+Test integration of plotter with Matplotlib drawer.
+"""
+
+from test.base import QiskitExperimentsTestCase
+
+import matplotlib
+
+from qiskit_experiments.visualization import MplDrawer
+
+from .mock_plotter import MockPlotter
+
+
+class TestPlotterAndMplDrawer(QiskitExperimentsTestCase):
+    """Test generic plotter with Matplotlib drawer."""
+
+    def test_end_to_end(self):
+        """Test whether plotter with MplDrawer returns a figure."""
+        plotter = MockPlotter(MplDrawer())
+        plotter.set_series_data(
+            "seriesA", x=[0, 1, 2, 3, 4, 5], y=[0, 1, 0, 1, 0, 1], z=[0.1, 0.1, 0.3, 0.4, 0.0]
+        )
+        fig = plotter.figure()
+
+        # Expect something
+        self.assertTrue(fig is not None)
+
+        # Expect a specific type
+        self.assertTrue(isinstance(fig, matplotlib.pyplot.Figure))
