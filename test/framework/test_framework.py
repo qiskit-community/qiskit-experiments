79da1f7122cff87a0ac571971c4cbc95919cddd0
Itamar Goldman <itamargoldman@gmail.com>
2022-10-23 16:53:24 +0300

    added check that the analysis data is save\over written as a function of "replace_results" option

--- test/framework/test_framework.py
+++ test/framework/test_framework.py
@@ -108,8 +108,14 @@ class TestFramework(QiskitExperimentsTestCase):
         with warnings.catch_warnings(record=True) as w:
             # Cause all warnings to always be triggered.
             warnings.simplefilter("always")
-            expdata2 = failed_analysis.run(expdata1, replace_results=True, seed=12345)
+            expdata2 = failed_analysis.run(expdata1, replace_results=True, seed=12345).block_for_results()
+
+            # check warning message raised.
             self.assertIn("Failed analysis for testing.", w[-1].message.args[0])
+            # check that the analysis is empty for the answer of the failed analysis.
+            self.assertEqual(expdata2.analysis_results(), [])
+            # confirming original analysis results is empty due to 'replace_results=True'
+            self.assertEqual(expdata1.analysis_results(), [])
 
     def test_failed_analysis_replace_results_false(self):
         """Test running analysis with replace_results=False"""
@@ -128,7 +134,13 @@ class TestFramework(QiskitExperimentsTestCase):
             # Cause all warnings to always be triggered.
             warnings.simplefilter("always")
             expdata2 = failed_analysis.run(expdata1, replace_results=False, seed=12345)
+
+            # check warning message raised
             self.assertIn("Failed analysis for testing.", w[-1].message.args[0])
+            # check that the analysis is empty for the answer of the failed analysis.
+            self.assertEqual(expdata2.analysis_results(), [])
+            # confirming original analysis results isn't empty due to 'replace_results=False'
+            self.assertNotEqual(expdata1.analysis_results(), [])
 
     def test_analysis_config(self):
         """Test analysis config dataclass"""
