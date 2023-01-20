3f8c2aa459e234f7fbf693235507bb0319dc6930
Christopher J. Wood <cjwood@us.ibm.com>
2023-01-20 12:55:27 -0500

    Update tomography kwargs (#998)
    * Renames `qubits` kwarg of tomography experiments to `physical_qubits` to be more explicit.
    * Renames `measurement_qubits` and `preparation_qubits` kwargs of tomography experiments to `measurement_indices` and `preparation_indices` respectively. This is to avoid confusion since these args refer to the circuit qubit index in physical physical_qubits to be measurement or prepared.
    * Adds `backend` and `analysis` kwargs to StateTomography and ProcessTomography
    * Allow setting None as tomography analysis. Changes the default value of the analysis init kwarg for tomography experiments from None to "default". This is so that None can be used as a user supplied value to disable analysis for an experiment.

--- test/library/tomography/test_process_tomography.py
+++ test/library/tomography/test_process_tomography.py
@@ -67,6 +67,19 @@ class TestProcessTomography(QiskitExperimentsTestCase):
                     fid, target_fid, places=6, msg=f"{fitter} result fidelity is incorrect"
                 )
 
+    def test_full_qpt_analysis_none(self):
+        """Test QPT experiment without analysis"""
+        seed = 4321
+        shots = 1000
+        # Generate tomography data without analysis
+        backend = AerSimulator(seed_simulator=seed, shots=shots)
+        target = qi.random_unitary(2, seed=seed)
+        exp = ProcessTomography(target, backend=backend, analysis=None)
+        self.assertEqual(exp.analysis, None)
+        expdata = exp.run()
+        self.assertExperimentDone(expdata)
+        self.assertFalse(expdata.analysis_results())
+
     def test_cvxpy_gaussian_lstsq_cx(self):
         """Test fitter with high fidelity threshold"""
         seed = 1234
@@ -100,7 +113,7 @@ class TestProcessTomography(QiskitExperimentsTestCase):
         )
 
     @ddt.data([0], [1], [2], [0, 1], [1, 0], [0, 2], [2, 0], [1, 2], [2, 1])
-    def test_exp_measurement_preparation_qubits(self, qubits):
+    def test_exp_measurement_preparation_indices(self, qubits):
         """Test subset measurement process tomography generation"""
         # Subsystem unitaries
         seed = 1111
@@ -113,7 +126,7 @@ class TestProcessTomography(QiskitExperimentsTestCase):
             circ.append(op, [i])
 
         num_meas = len(qubits)
-        exp = ProcessTomography(circ, measurement_qubits=qubits, preparation_qubits=qubits)
+        exp = ProcessTomography(circ, measurement_indices=qubits, preparation_indices=qubits)
         tomo_circuits = exp.circuits()
 
         # Check correct number of circuits are generated
@@ -150,7 +163,7 @@ class TestProcessTomography(QiskitExperimentsTestCase):
             circ.append(op, [i])
 
         exp = ProcessTomography(
-            circ, measurement_qubits=[meas_qubit], preparation_qubits=[prep_qubit]
+            circ, measurement_indices=[meas_qubit], preparation_indices=[prep_qubit]
         )
         backend = AerSimulator(seed_simulator=9000)
         expdata = exp.run(backend, shots=5000)
@@ -182,7 +195,7 @@ class TestProcessTomography(QiskitExperimentsTestCase):
             circ.append(op, [i])
 
         exp = ProcessTomography(
-            circ, measurement_qubits=meas_qubits, preparation_qubits=prep_qubits
+            circ, measurement_indices=meas_qubits, preparation_indices=prep_qubits
         )
         backend = AerSimulator(seed_simulator=9000)
         expdata = exp.run(backend, shots=5000)
@@ -224,7 +237,7 @@ class TestProcessTomography(QiskitExperimentsTestCase):
 
         # Run
         backend = AerSimulator(seed_simulator=9000)
-        exp = ProcessTomography(circ, measurement_qubits=qubits, preparation_qubits=qubits)
+        exp = ProcessTomography(circ, measurement_indices=qubits, preparation_indices=qubits)
         expdata = exp.run(backend)
         self.assertExperimentDone(expdata)
         results = expdata.analysis_results()
@@ -250,7 +263,7 @@ class TestProcessTomography(QiskitExperimentsTestCase):
         # Teleport qubit 0 -> 2
         backend = AerSimulator(seed_simulator=9000)
         exp = ProcessTomography(
-            teleport_circuit(flatten_creg), measurement_qubits=[2], preparation_qubits=[0]
+            teleport_circuit(flatten_creg), measurement_indices=[2], preparation_indices=[0]
         )
         expdata = exp.run(backend, shots=1000)
         self.assertExperimentDone(expdata)
@@ -274,8 +287,8 @@ class TestProcessTomography(QiskitExperimentsTestCase):
         backend = AerSimulator(seed_simulator=9000)
         exp = ProcessTomography(
             teleport_bell_circuit(flatten_creg),
-            measurement_qubits=[2, 3],
-            preparation_qubits=[0, 3],
+            measurement_indices=[2, 3],
+            preparation_indices=[0, 3],
         )
         expdata = exp.run(backend, shots=1000)
         self.assertExperimentDone(expdata)
@@ -300,7 +313,9 @@ class TestProcessTomography(QiskitExperimentsTestCase):
 
     def test_experiment_config(self):
         """Test converting to and from config works"""
-        exp = ProcessTomography(teleport_circuit(), measurement_qubits=[2], preparation_qubits=[0])
+        exp = ProcessTomography(
+            teleport_circuit(), measurement_indices=[2], preparation_indices=[0]
+        )
         loaded_exp = ProcessTomography.from_config(exp.config())
         self.assertNotEqual(exp, loaded_exp)
         self.assertTrue(self.json_equiv(exp, loaded_exp))
