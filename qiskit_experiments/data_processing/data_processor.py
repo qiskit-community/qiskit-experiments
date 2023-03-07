18062d9ea015ffaf8c862b628e99453b9cdf732b
Will Shanks <willshanks@us.ibm.com>
2023-03-07 16:03:48 -0500

    Suppress numpy warning during uarray creation

--- qiskit_experiments/data_processing/data_processor.py
+++ qiskit_experiments/data_processing/data_processor.py
@@ -261,10 +261,12 @@ class DataProcessor:
             # The output data format is a standard ndarray with dtype=object with
             # arbitrary shape [n_circuits, ...] depending on the measurement setup.
             nominal_values = np.asarray(data_to_process, float)
-            return unp.uarray(
-                nominal_values=nominal_values,
-                std_devs=np.full_like(nominal_values, np.nan, dtype=float),
-            )
+            with np.errstate(invalid="ignore"):
+                uarray = unp.uarray(
+                    nominal_values=nominal_values,
+                    std_devs=np.full_like(nominal_values, np.nan, dtype=float),
+                )
+            return uarray
         else:
             # Likely level2 counts or level2 memory data. Cannot be typecasted to ufloat.
             # The output data format is a standard ndarray with dtype=object with
