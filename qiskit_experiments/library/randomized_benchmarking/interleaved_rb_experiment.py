9fe27bf65a4a939c43db49a4e205a713568377ae
Toshinari Itoko <15028342+itoko@users.noreply.github.com>
2023-03-27 10:40:06 +0900

    Refactor RB unit tests (#1089)
    ### Summary
    
    Separate one big `test_randomized_benchmarking.py` file into three files
    (`test_standard_rb.py`, `test_interleaved_rb.py` and
    `test_rb_analysis.py`) for the faster individual tests for `StandarRB`,
    `InterleavedRB` or `RBAnalysis` in local env. And rename
    `rb_experiment.py` to `standard_rb.py` to follow naming convention.
    
    ### Details and comments
    - Rename `rb_experiment.py` to `standard_rb.py`
    - Split `test_randomized_benchmarking.py` into `test_standard_rb.py`,
    `test_interleaved_rb.py` and `test_rb_analysis.py`
    
    New test files `test_standard_rb.py` and `test_interleaved_rb.py` are
    added. They correspond with two existing source files `standard_rb.py`
    and `interleaved_rb_experiment.py` and they store unit tests for
    `StandardRB` and `InterleavedRB`. After that, all tests in
    `test_randomized_benchmarking.py` are for `RBAnalysis` class so renamed
    it `test_rb_analysis`.

--- qiskit_experiments/library/randomized_benchmarking/interleaved_rb_experiment.py
+++ qiskit_experiments/library/randomized_benchmarking/interleaved_rb_experiment.py
@@ -29,7 +29,7 @@ from qiskit_experiments.framework.backend_timing import BackendTiming
 from .clifford_utils import _truncate_inactive_qubits
 from .clifford_utils import num_from_1q_circuit, num_from_2q_circuit
 from .interleaved_rb_analysis import InterleavedRBAnalysis
-from .rb_experiment import StandardRB, SequenceElementType
+from .standard_rb import StandardRB, SequenceElementType
 
 
 class InterleavedRB(StandardRB):
