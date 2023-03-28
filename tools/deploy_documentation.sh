1e54689580dfc622d56351938ee385c096fb448f
Matthew Treinish <mtreinish@kortar.org>
2023-03-28 18:52:32 -0400

    Fix IFS setting in doc deploy script (#1110)
    ### Summary
    
    The 0.5.0 docs publishing job resulted in failure because the openssl
    command to decrypt the credentials file for uploading the docs errored.
    The IFS (internal field separator) variable was getting set too broadly
    at the the level of the entire script instead of just the tag parsing.
    This was causing the passing of the secrets for decrypting the
    credential files to fail. This commit updates the IFS usage so it's not
    affecting the other commands after we split the tag version. This should
    fix the deploy job for future releases.
    
    ### Details and comments
    
    (cherry picked from commit 8fe22c965a87d377ffcc4b68eb02c2e6754fcad4)
    
    # Conflicts:
    #	tools/deploy_documentation.sh

--- tools/deploy_documentation.sh
+++ tools/deploy_documentation.sh
@@ -26,6 +26,14 @@ tox -edocs
 echo "show current dir: "
 pwd
 
+<<<<<<< HEAD
+=======
+CURRENT_TAG=`git describe --abbrev=0`
+IFS=. read -ra VERSION <<< "$CURRENT_TAG"
+STABLE_VERSION=${VERSION[0]}.${VERSION[1]}
+echo "Building for stable version $STABLE_VERSION"
+
+>>>>>>> 8fe22c9 (Fix IFS setting in doc deploy script (#1110))
 # Push to qiskit.org website
 openssl aes-256-cbc -K $encrypted_rclone_key -iv $encrypted_rclone_iv -in tools/rclone.conf.enc -out $RCLONE_CONFIG_PATH -d
 echo "Pushing built docs to website"
