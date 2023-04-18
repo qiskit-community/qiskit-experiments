3e09dc2dda9681ee10b3911f231f5a8cc83eb9b3
Helena Zhang <Helena.Zhang@ibm.com>
2023-04-18 12:14:46 -0400

    Update release notes and documentation (#1147)
    ### Summary
    
    This PR formats the release notes for 0.5.1 and updates places in the
    `README` and `setup.py` that were still configured for Terra instead of
    Experiments, in addition to some formatting fixes and adding
    `RoughEFFrequencyCal` to the documentation. There will be another PR to
    the 0.5 branch to make the 0.5.1 release notes and bump the patch
    version.

--- test/fake_experiment.py
+++ test/fake_experiment.py
@@ -43,11 +43,11 @@ class FakeExperiment(BaseExperiment):
         options.dummyoption = None
         return options
 
-    def __init__(self, qubits=None):
+    def __init__(self, physical_qubits=None):
         """Initialise the fake experiment."""
-        if qubits is None:
-            qubits = [0]
-        super().__init__(qubits, analysis=FakeAnalysis())
+        if physical_qubits is None:
+            physical_qubits = [0]
+        super().__init__(physical_qubits, analysis=FakeAnalysis())
 
     def circuits(self):
         """Fake circuits."""
