99105a682d0f14bb9b6046430d83a30b86168c25
Helena Zhang <Helena.Zhang@ibm.com>
2023-04-18 20:45:43 -0400

    0.5.1 release (#1149)
    ### Summary
    
    This PR bumps the version to 0.5.1 and makes the release note.

--- docs/conf.py
+++ docs/conf.py
@@ -38,7 +38,7 @@ os.environ["QISKIT_DOCS"] = "TRUE"
 # The short X.Y version
 version = "0.5"
 # The full version, including alpha/beta/rc tags
-release = "0.5.0"
+release = "0.5.1"
 project = f"Qiskit Experiments {version}"
 copyright = f"2021-{datetime.date.today().year}, Qiskit Development Team"  # pylint: disable=redefined-builtin
 author = "Qiskit Development Team"
