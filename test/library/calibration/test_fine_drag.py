e19e45c8717753ea16ed2855976b411fcffea71c
Will Shanks <willshanks@us.ibm.com>
2023-05-03 16:28:07 -0400

    Update calibration tests to test _transpiled_circuits (#1157)
    ### Summary
    
    Previously, the tests were calling `transpile()` on the output of
    `experiment.circuits()` even though
    `BaseCalibrationExperiment._transpiled_circuits()` uses a custom pass
    manager and does not call `transpile()` itself.

--- test/library/calibration/test_fine_drag.py
+++ test/library/calibration/test_fine_drag.py
@@ -13,10 +13,9 @@
 """Test fine drag calibration experiment."""
 
 from test.base import QiskitExperimentsTestCase
-import copy
 import numpy as np
 
-from qiskit import pulse, transpile
+from qiskit import pulse
 from qiskit.circuit import Gate
 from qiskit.providers.fake_provider import FakeArmonkV2
 
@@ -102,13 +101,7 @@ class TestFineDragCal(QiskitExperimentsTestCase):
 
         drag_cal = FineDragCal([0], self.cals, "x", self.backend)
 
-        transpile_opts = copy.copy(drag_cal.transpile_options.__dict__)
-        transpile_opts["initial_layout"] = list(drag_cal.physical_qubits)
-        transpile_opts["optimization_level"] = 0
-
-        circs = transpile(
-            drag_cal.circuits(), inst_map=self.cals.default_inst_map, **transpile_opts
-        )
+        circs = drag_cal._transpiled_circuits()
 
         with pulse.build(name="x") as expected_x:
             pulse.play(pulse.Drag(160, 0.5, 40, 0), pulse.DriveChannel(0))
@@ -127,13 +120,7 @@ class TestFineDragCal(QiskitExperimentsTestCase):
         target_angle = np.pi
         new_beta = -np.sqrt(np.pi) * d_theta * sigma / target_angle**2
 
-        transpile_opts = copy.copy(drag_cal.transpile_options.__dict__)
-        transpile_opts["initial_layout"] = list(drag_cal.physical_qubits)
-        transpile_opts["optimization_level"] = 0
-
-        circs = transpile(
-            drag_cal.circuits(), inst_map=self.cals.default_inst_map, **transpile_opts
-        )
+        circs = drag_cal._transpiled_circuits()
 
         x_cal = circs[5].calibrations["x"][((0,), ())]
 
