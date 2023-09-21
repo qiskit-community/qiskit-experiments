89892c9cee33f07229bc716c577ee650d12ca598
Itamar Goldman <itamargoldman@gmail.com>
2023-09-21 09:28:04 +0300

    added call for `super` to keep old flow.

--- qiskit_experiments/framework/composite/composite_analysis.py
+++ qiskit_experiments/framework/composite/composite_analysis.py
@@ -99,6 +99,7 @@ class CompositeAnalysis(BaseAnalysis):
     def set_options(self, **fields):
         """Set the analysis options for the experiment. If the `broadcast` key-word is passed, the
         analysis option will cascade to the children experiment."""
+        super().set_options(**fields)
         if fields.get("broadcast", None):
             for sub_analysis in self._analyses:
                 sub_analysis.set_options(**fields)
