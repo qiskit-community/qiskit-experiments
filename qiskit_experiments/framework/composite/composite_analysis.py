a879a2ce9a1f133333aa992b795b945bc6b722d9
ItamarGoldman <itamargoldman@gmail.com>
2023-10-16 09:59:08 +0300

    fixed the description

--- qiskit_experiments/framework/composite/composite_analysis.py
+++ qiskit_experiments/framework/composite/composite_analysis.py
@@ -97,8 +97,8 @@ class CompositeAnalysis(BaseAnalysis):
         return self._analyses[index]
 
     def set_options(self, **fields):
-        """Set the analysis options for the experiment. If the `broadcast` argument is passed and set to
-        `True`, the analysis options will cascade to the children experiments."""
+        """Set the analysis options for the experiment. If the `broadcast` argument set to `True`, the
+        analysis options will cascade to the child experiments."""
         super().set_options(**fields)
         if fields.get("broadcast", None):
             for sub_analysis in self._analyses:
