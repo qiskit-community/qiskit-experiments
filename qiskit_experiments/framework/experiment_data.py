6919ffb1ef2a31b05f85e180ea352a1f9b77b827
ItamarGoldman <itamargoldman@gmail.com>
2023-11-09 12:09:37 +0200

    Added a check for existing data
    Added a check for existing data in the `ExperimentData` object to not spam the user if a simulator is used.

--- qiskit_experiments/framework/experiment_data.py
+++ qiskit_experiments/framework/experiment_data.py
@@ -1070,8 +1070,9 @@ class ExperimentData:
     def _retrieve_data(self):
         """Retrieve job data if missing experiment data."""
         # Get job results if missing in experiment data.
-        if self.provider is None:
-            LOG.warning("provider is None, no data was retrieved.")
+        if self.provider is None and not self._result_data.copy():
+            LOG.warning("provider is None and there is no result data that are stored."
+                        " no data was retrieved.")
             return
         retrieved_jobs = {}
         jobs_to_retrieve = []  # the list of all jobs to retrieve from the server
