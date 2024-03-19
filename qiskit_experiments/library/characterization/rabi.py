8455a71b3fd95ed4c4d5787f043412714d9530ab
Naohito Nakazawa <117684192+Naohnakazawa@users.noreply.github.com>
2024-03-20 05:19:58 +0900

    Rabi & EFRabi update

--- qiskit_experiments/library/characterization/rabi.py
+++ qiskit_experiments/library/characterization/rabi.py
@@ -63,7 +63,7 @@ class Rabi(BaseExperiment, RestlessMixin):
 
             # backend
             from qiskit_experiments.test.pulse_backend import SingleTransmonTestBackend
-            backend = SingleTransmonTestBackend(5.2e9,-.25e9, 1e9, 0.8e9, 1e4, noise=False, seed=199)
+            backend = SingleTransmonTestBackend(5.2e9,-0.25e9, 1e9, 0.8e9, 1e4, noise=True, seed=199)
 
         .. jupyter-execute::
 
@@ -233,7 +233,7 @@ class EFRabi(Rabi):
 
             # backend
             from qiskit_experiments.test.pulse_backend import SingleTransmonTestBackend
-            backend = SingleTransmonTestBackend(5.2e9,-.25e9, 1e9, 0.8e9, 1e4, noise=False, seed=199)
+            backend = SingleTransmonTestBackend(5.2e9, -0.25e9, 1e9, 0.8e9, 1e4, noise=True, seed=199)
 
         .. jupyter-execute::
 
