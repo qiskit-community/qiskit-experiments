98a67a9ab4ccb0874888da30f812a52019241959
Naohito Nakazawa <117684192+Naohnakazawa@users.noreply.github.com>
2024-10-29 16:01:48 +0900

    update calibration/half_angle_cal.py

--- qiskit_experiments/library/calibration/half_angle_cal.py
+++ qiskit_experiments/library/calibration/half_angle_cal.py
@@ -43,7 +43,7 @@ class HalfAngleCal(BaseCalibrationExperiment, HalfAngle):
             from qiskit import pulse
             from qiskit_experiments.calibration_management.calibrations import Calibrations
             from qiskit_experiments.calibration_management.basis_gate_library
-	    import FixedFrequencyTransmon
+            import FixedFrequencyTransmon
             from qiskit_experiments.library.calibration.half_angle_cal import HalfAngleCal
 
             library = FixedFrequencyTransmon(default_values={"duration": 640})
