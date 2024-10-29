f0ee881cfa5484e4055633f6c3ad81ffba8f3b28
Naohito Nakazawa <117684192+Naohnakazawa@users.noreply.github.com>
2024-10-29 14:36:39 +0900

    update tomography/mit_qpt_experiment.py

--- qiskit_experiments/library/tomography/mit_qpt_experiment.py
+++ qiskit_experiments/library/tomography/mit_qpt_experiment.py
@@ -75,7 +75,7 @@ class MitigatedProcessTomography(BatchExperiment):
                 qc_ghz.cx(0, i)
 
             mitqptexp = MitigatedProcessTomography(qc_ghz)
-	    mitqptexp.set_run_options(shots=1000)
+            mitqptexp.set_run_options(shots=1000)
             mitqptdata = mitqptexp.run(backend=backend, seed_simulator=100).block_for_results()
             mitigated_choi_out = mitqptdata.analysis_results("state").value
 
